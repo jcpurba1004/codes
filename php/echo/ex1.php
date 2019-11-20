346e82626553466600d9491012d4aed808953953
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2019-11-19 20:40:13 -0800

    Add echo php example 1
    Add echo php example 1

---
+++ php/echo/ex1.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hello, World!</title>
+	<style type="text/css">
+	.bold {
+		font-weight: bolder;
+	}
+	</style>
+</head>
+<body>
+
+<?php print date("Y-m-d"); ?> <!-- Example of data function -->
+<p>The following was created by PHP: <br>
+
+<?php
+/*
+ *	Filename: ch01.php
+ *	Book reference: Script 1.5
+ *	Created by: Jeremiah Purba
+ */
+
+echo "<h1>Jeremiah Purba</h1>";
+echo "<ul>Three of my favorite hobbies are:";
+echo "<li>Swimming</li>";
+echo "<li>Exercise at gym</li>";
+echo "<li>Singing</li></ul>";
+
+?>
+<!-- This is an HTML comment. -->
+</p>
+</body>
+</html>
