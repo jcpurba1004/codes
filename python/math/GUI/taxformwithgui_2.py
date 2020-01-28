133ba6a835db7a3d1c0f0b1d7589f94e882649d0
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-01-27 17:14:08 -0800

    Create taxformwithgui.py and taxformwithgui_2.py
    Create taxformwithgui.py and taxformwithgui_2.py

--- python/math/GUI/taxformwithgui_2.py
+++ python/math/GUI/taxformwithgui_2.py
@@ -17,28 +17,21 @@ class TaxCalculator(EasyFrame):
     def __init__(self):
         """Sets up the window and the widgets."""
         EasyFrame.__init__(self, title = "Tax Calculator")
-  
+
         # Label and field for the income
         self.addLabel(text = "Income",
                       row = 0, column = 0)
-        self.incomeField = self.addFloatField(value = 0.0,
-                                              row = 0,
-                                              column = 1)
- 
+#        self.incomeField = self.addFloatField(value = = 0.0,
+#                                              row = 0,
+#                                              column = 1)
+
         # Label and field for the number of dependents
         self.addLabel(text = "Dependents",
                       row = 1, column = 0)
         self.depField = self.addIntegerField(value = 0,
                                              row = 1,
                                              column = 1)
-  
-        # Label and field for the exemption amount
-        #self.addLabel(text = "Exemption amount",
-        #              row = 2, column = 0)
-        #self.exeField = self.addFloatField(value = 0.0,
-        #                                   row = 2,
-        #                                   column = 1)
-        # The command button
+
         self.addButton(text = "Compute", row = 2, column = 0,
                        columnspan = 2, command = self.computeTax)
         # Label and field for the tax
@@ -48,3 +41,37 @@ class TaxCalculator(EasyFrame):
                                            row = 3,
                                            column = 1,
                                            precision = 2)
+
+    # The event handler method for the button
+    def computeTax(self):
+        """Obtains the data form the input field and uses
+        them to compute the tax, which is sent to the
+        output field."""
+#        DEDUCTION = 10000
+#        TAXRATE   = 0.2     # flat tax rate 20%
+#        EXEMPTION = 3000
+
+        # Obtain and validate the inputs
+#        income = self.incomeField.getNumber()
+
+#        if income == 0:
+#            return
+
+        # Compute the tax
+#        dependents = self.depField.getNumber()
+#        totalExemp = dependents * EXEMPTION
+#        totalSubtract = totalExemp + DEDUCTION
+#        netIncome = income - totalSubtract
+#        totalTax = TAXRATE * netIncome
+
+        # Output the result while preserving read-only status
+        self.taxField["state"] = "normal"
+#        self.taxField.setNumber(totalTax)
+        self.taxField["state"] = "disabled"
+
+
+def main():
+    TaxCalculator().mainloop()
+
+if __name__ == "__main__":
+    main()  
