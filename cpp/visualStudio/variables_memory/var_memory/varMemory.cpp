3ef2b577a4c2c4c6fd4d983aaab2a0ec966a256d
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-02-11 09:38:07 -0800

    Create rectangle.cpp for the area.
    Create rectangle.cpp for the area.

---
+++ cpp/visualStudio/variables_memory/var_memory/varMemory.cpp
@@ -0,0 +1,19 @@
+//***************************************************************
+// Given the length and width of a rectangle, this C++ program
+// computers and outputs the perimeter and area of the rectangle.
+//***************************************************************
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+
+	cout << "Hello WOrld"<< endl;
+
+	
+
+	return 0;
+}
