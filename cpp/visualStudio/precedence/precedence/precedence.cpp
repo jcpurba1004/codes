625930ebcc6298ce81da52a91ddf8401c197f6db
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-02-11 22:49:32 -0800

    Add precedence for the calculation.
    Add precedence for the calculation.

---
+++ cpp/visualStudio/precedence/precedence/precedence.cpp
@@ -0,0 +1,43 @@
+// precedence.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// This program illustrates how arithmetic operators work
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "2 + 5 = " << 2 + 5 << endl;
+    cout << "13 + 89 = " << 13 + 89 << endl;
+    cout << "34 - 20 = " << 34 - 20 << endl;
+    cout << "45 - 90 = " << 45 - 90 << endl;
+    cout << "2 * 7 = " << 2 * 7 << endl;
+    cout << "5 / 2 = " << 5 / 2 << endl;
+    cout << "14 / 7 = " << 14 / 7 << endl;
+    cout << "34 % 5 = " << 34 % 5 << endl;
+    cout << "4 % 6 = " << 4 % 6 << endl << endl;
+
+    cout << "5.0 + 3.5 = " << 5.0 + 3.5 << endl;
+    cout << "3.0 + 9.4 = " << 3.0 + 9.4 << endl;
+    cout << "16.3 - 5.2 = " << 16.3 - 5.2 << endl;
+
+    cout << "4.2 * 2.5 = " << 4.2 * 2.5 << endl;
+    cout << "5.0 / 2.0 = " << 5.0 / 2.0 << endl;
+    cout << "34.5 / 6.0 = " << 34.5 / 6.0 << endl;
+    cout << "34.5 / 6.5 = " << 34.5 / 6.5 << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
