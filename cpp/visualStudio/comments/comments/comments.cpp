c18ad5fbae5c67048e34df6aed0da264e9f744dc
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-02-13 10:03:58 -0800

    Create comments example.
    Create comments example.

---
+++ cpp/visualStudio/comments/comments/comments.cpp
@@ -0,0 +1,32 @@
+// comments.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//comments, if needed
+
+preprocessor directives to include header files
+
+using statement
+
+named constants, if needed
+
+int main()
+{
+    statement_1
+       *
+       *
+       *
+    statement_n
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
