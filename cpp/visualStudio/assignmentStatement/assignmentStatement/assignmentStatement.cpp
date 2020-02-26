0dbae4f1e09e31475679c4d04490cd23425387d1
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-02-25 21:47:45 -0800

    Create assignmentStatement.
    Create assignmentStatement.

---
+++ cpp/visualStudio/assignmentStatement/assignmentStatement/assignmentStatement.cpp
@@ -0,0 +1,46 @@
+// assignmentStatement.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// This program illustrates how data in the variables are
+// manipulated.
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    double sale;
+    char first;
+    string str;
+
+    num1 = 4;
+    cout << "num1 = " << num1 << endl;
+
+    num2 = 4 * 5 - 11;
+    cout << "num2 = " << num2 << endl;
+
+    sale = 0.02 * 1000;
+    cout << "sale = " << sale << endl;
+
+    first = 'D';
+    cout << "first = " << first << endl;
+
+    str = "It is a sunny day.";
+    cout << "str = " << str << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
