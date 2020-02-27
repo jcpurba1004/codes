fa36512cc5d6bc82d798a4411c12929a7827fb01
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-02-26 17:43:44 -0800

    Update the project and make it into function.
    Update the project and make it into function.

--- cpp/visualStudio/feet2Inches/feet2Inches/feet2Inches.cpp
+++ cpp/visualStudio/feet2Inches/feet2Inches/feet2Inches.cpp
@@ -2,17 +2,49 @@
 //
 
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
+        //named constants
+const double CENTIMETERS_PER_INCH = 2.54;
+const int INCHES_PER_FOOT = 12;
+
+void inches2Centimeters(void) {
+    
+        //declare variables
+    int feet, inches;
+    int totalInches;
+    double centimeters;
+
+        //Statements: Step 1 - Step 7
+    cout << "Enter two integers, one for feet and "
+         << "one for inches: ";                       //Step 1
+    cin >> feet >> inches;                            //Step 2               
+    cout << endl;
+
+    cout << "The numbers you entered are ";
+    cout << feet << " for feet and " << inches;
+    cout << " for inches. " << endl;                  //Step 3
+
+    totalInches = INCHES_PER_FOOT * feet + inches;    //Step 4
+
+    cout << "The total number of inches = ";
+    cout << totalInches;
+    cout << endl;                      //Step 5
+
+    centimeters = CENTIMETERS_PER_INCH * totalInches; //Step 6
+
+    cout << "The number of centimeters = ";
+    cout << centimeters << endl;                      //Step 7
+
+}
+
+void inchesCalculation(void) {
     int inches; //variables to store total inches
 
     inches = 100; //store 100 in the variable inches
 
     cout << inches << " inch(es) = ";
-                              //inches and the equal sign
+    //inches and the equal sign
     cout << inches / 12 << " feet (foot) and "; //output maximum
                                //number of feet (foot)
     cout << inches % 12 << " inch(es)" << endl; //output
@@ -30,6 +62,12 @@ int main()
                                //number of feet (foot)
     cout << inches % 12 << " inch(es)" << endl; //output
                                //remaining inches
+}
+
+int main()
+{
+    inchesCalculation();
+    inches2Centimeters();
 
     return 0;
 }
