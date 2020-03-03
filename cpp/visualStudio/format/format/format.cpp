19cf4859f0a9b8ea07abe6071dd8eda53e528f41
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-02 22:02:54 -0800

    Edited format.cpp
    Edited format.cpp

--- cpp/visualStudio/format/format/format.cpp
+++ cpp/visualStudio/format/format/format.cpp
@@ -10,16 +10,28 @@ using namespace std;
 
 int main()
 {
-int num; double height;
-string name;
-cout << "Enter an integer: "; cin >> num; cout << endl;
-    cout << "num: " << num << endl;
-cout << "Enter the first name: "; cin>>name;
-    cout << endl; cout <<"Enter the height: ";
-cin >> height; cout << endl;
+    int num; 
+    double height;
+    string name;
 
-cout << "Name: " << name << endl; cout << "Height: "
-<< height; cout << endl; return 0;
+    cout << "Enter an integer: "; 
+    cin >> num; 
+    cout << endl;
+
+    cout << "num: " << num << endl;
+
+    cout << "Enter the first name: "; 
+    cin >> name;
+    cout << endl; 
+
+    cout << "Enter the height: ";
+    cin >> height; 
+    cout << endl;
+
+    cout << "Name: " << name << endl; 
+    cout << "Height: " << height << endl; 
+    
+    return 0;
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
