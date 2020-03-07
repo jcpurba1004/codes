f89c1586827671725d84d94b6f98c45409825288
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-07 14:08:23 -0800

    Add unit3-7 example.
    Add unit3-7 example.

---
+++ cpp/visualStudio/unit3-7/unit3-7/unit3-7.cpp
@@ -0,0 +1,49 @@
+// unit3-7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//Functions peek and putback
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char ch;
+
+    cout << "line 6: Enter a string: ";
+    cin.get(ch);
+    cout << endl;
+    cout << "Line 9: After first cin.get(ch); "
+         << "ch = " << ch << endl;
+
+    cin.get(ch);
+    cout << "Line 11: After second cin.get(ch); "
+         << "ch = " << ch << endl;
+
+    cin.putback(ch);
+    cin.get(ch);
+    cout << "Line 14: After putback and then "
+         << "cin.get(ch); ch = " << ch << endl;
+
+    ch = cin.peek();
+    cout << "Line 16: After cin.peek(); ch = "
+         << ch << endl;
+
+    cin.get(ch);
+    cout << "Line 18: After cin.get(ch); ch = "
+         << ch << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
