37ae72787a3bda453bbb291d81d4b4eb9b96deec
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-07 14:34:21 -0800

    Add unit3-9 example.
    Add unit3-9 example.

---
+++ cpp/visualStudio/unit3-9/unit3-9/unit3-9.cpp
@@ -0,0 +1,54 @@
+// unit3-9.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+
+//Input failure and the clear function
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string name;
+    int age = 0;
+    int weight = 0;
+    double height = 0.0;
+
+    cout << "Line 10: Enter name, age, weight and "
+         << "height: ";
+    cin >> name >> age >> weight >> height;
+    cout << endl;
+
+    cout << "Line 13: Name: " << name << endl;
+    cout << "Line 14: Age: " << age << endl;
+    cout << "Line 15: Weight: " << weight << endl;
+    cout << "Line 16: Height: " << height << endl;
+
+    cin.clear();                 //Restore input stream;
+
+    cin.ignore(200, '\n');           //Clear the buffer;
+
+    cout << "\nLine 19: Enter name, age, weight, "
+         << "and height: ";
+    cin >> name >> age >> weight >> height;
+    cout << endl;
+
+    cout << "Line 22: Name: " << name << endl;
+    cout << "Line 23: Age: " << age << endl;
+    cout << "Line 24: Weight: " << weight << endl;
+    cout << "Line 25: Height: " << height << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
