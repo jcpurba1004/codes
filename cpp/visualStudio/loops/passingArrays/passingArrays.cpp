a20d972911e243fdad687a09ea5672f5b46ff390
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-16 14:12:42 -0800

    Add passingArrays.cpp
    Add passingArrays.cpp

---
+++ cpp/visualStudio/loops/passingArrays/passingArrays.cpp
@@ -0,0 +1,31 @@
+// passingArrays.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int bucky[3] = { 20, 54, 675 };
+	int jessica[6] = { 54,24,7,8,9,99 };
+
+	printArray(jessica, 6);
+}
+
+void printArray(int theArray[], int sizeOfArray) {
+
+	for (int x = 0; x < sizeOfArray; x++) {
+		cout << theArray[x] << endl;
+	}
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
