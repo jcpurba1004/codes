51723cc1c35f07ee884441e8d8afafbe6409b1aa
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-17 08:12:03 -0800

    Update constantObjects.cpp
    Update constantObjects.cpp

---
+++ cpp/visualStudio/operations/constObjects/Sally.h
@@ -0,0 +1,15 @@
+#pragma once
+#ifndef Sally_H
+#define Sally_H
+
+class Sally
+{
+	public:
+		Sally();
+		void printShiz();
+		void printShiz2() const;
+	protected:
+	private:
+};
+
+#endif // Sally_H
