77a996ce74a50f449721bd54e29869b5703a3316
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-21 14:59:07 -0800

    Create month example.
    Create month example.

---
+++ cpp/visualStudio/month/month/month.cpp
@@ -0,0 +1,48 @@
+// month.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    if (month == 1)
+        cout << "January" << endl;
+    else if (month == 2)
+        cout << "February" << endl;
+    else if (month == 3)
+        cout << "March" << endl;
+    else if (month == 4)
+        cout << "April" << endl;
+    else if (month == 5)
+        cout << "May" << endl;
+    else if (month == 6)
+        cout << "June" << endl;
+
+    if (month == 1)
+        cout << "January" << endl;
+    if (month == 2)
+        cout << "February" << endl;
+    if (month == 3)
+        cout << "March" << endl;
+    if (month == 4)
+        cout << "April" << endl;
+    if (month == 5)
+        cout << "May" << endl;
+    if (month == 6)
+        cout << "June" << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
