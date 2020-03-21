375e4cccf38d6f43da7213cebc5eaf450389f3e6
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-21 14:35:22 -0800

    Create unit4-17 example.
    Create unit4-17 example.

---
+++ cpp/visualStudio/unit4-17/unit4-17/unit4-17.cpp
@@ -0,0 +1,33 @@
+// unit4-17.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    if (score >= 90)
+        cout << "The grade is A." << endl;
+    else if (score >= 80)
+        cout << "The grade is B." << endl;
+    else if (score >= 70)
+        cout << "The grade is C." << endl;
+    else if (score >= 60)
+        cout << "The grade is D." << endl;
+    else (score >= 90)
+        cout << "The grade is A." << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
