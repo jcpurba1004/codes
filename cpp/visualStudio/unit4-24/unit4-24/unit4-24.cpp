9992e8ed60d7511f3c0c5d73cac2d7f0b2193b90
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-22 09:53:38 -0800

    Add unit4-24 example.
    Add unit4-24 example.

---
+++ cpp/visualStudio/unit4-24/unit4-24/unit4-24.cpp
@@ -0,0 +1,47 @@
+// unit4-24.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    switch statement:
+    {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        grade = 'F';
+        break;
+    case 6:
+        grade = 'D';
+        break;
+    case 7:
+        grade = 'C';
+        break;
+    case 8:
+        grade = 'B';
+        break;
+    case 9:
+    case 10:
+        grade = 'A';
+        break;
+    default:
+        cout << "Invalid test score." << endl;
+
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
