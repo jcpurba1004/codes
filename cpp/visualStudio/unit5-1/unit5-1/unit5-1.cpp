5e62d6a35b3f25edd725669461b5268d45047ae9
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-24 15:02:51 -0800

    Create unit5-1 example.
    Create unit5-1 example.

---
+++ cpp/visualStudio/unit5-1/unit5-1/unit5-1.cpp
@@ -0,0 +1,61 @@
+// unit5-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int calBurnedDay1, calBurnedDay2, calBurnedDay3,
+        calBurnedDay4, calBurnedDay5, calBurnedDay6,
+        calBurnedDay7;  
+
+    cout << "Enter calories burned day 1: ";
+    cin >> calBurnedDay1;
+    cout << endl;
+
+    cout << "Enter calories burned day 2: ";
+    cin >> calBurnedDay2;
+    cout << endl;
+
+    cout << "Enter calories burned day 3: ";
+    cin >> calBurnedDay3;
+    cout << endl;
+
+    cout << "Enter calories burned day 4: ";
+    cin >> calBurnedDay4;
+    cout << endl;
+
+    cout << "Enter calories burned day 5: ";
+    cin >> calBurnedDay5;
+    cout << endl;
+
+    cout << "Enter calories burned day 6: ";
+    cin >> calBurnedDay6;
+    cout << endl;
+
+    cout << "Enter calories burned day 7: ";
+    cin >> calBurnedDay7;
+    cout << endl;
+
+    calBurnedInAWeek = calBurnedDay1 + calBurnedDay2 + calBurnedDay3
+                     + calBurnedDay4 + calBurnedDay5 + calBurnedDay6
+                     + calBurnedDay7;
+
+    cout << "Average number of calories burned each day: "
+         << calBurnedInAWeek / 7 << endl;
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
