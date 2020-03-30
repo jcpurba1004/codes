0d008dd4edc473dd149b25e15d0ce28c1e9ea4f9
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-30 15:45:10 -0800

    Create unit6-18 example.
    Create unit6-18 example.

---
+++ cpp/visualStudio/unit6-18/unit6-18/unit6-18.cpp
@@ -0,0 +1,43 @@
+// unit6-18.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//Program: Static and automatic variables
+
+#include <iostream>
+
+using namespace std;
+
+void test();
+
+int main()
+{
+    int count;
+
+    for (count = 1; count <= 5; count++)
+        test();
+
+    return 0;
+}
+
+void test()
+{
+    static int x = 0;
+    int y = 10;
+
+    x = x + 2;
+    y = y + 1;
+
+    cout << "Inside test x = " << x << " and y = "
+         << y << endl;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
