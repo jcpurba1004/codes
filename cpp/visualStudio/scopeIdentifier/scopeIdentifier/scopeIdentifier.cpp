cdb96767a053db6e4ab01f966b6829a456d5a265
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-03-30 14:30:07 -0800

    Add scopeIdentifier example.
    Add scopeIdentifier example.

---
+++ cpp/visualStudio/scopeIdentifier/scopeIdentifier/scopeIdentifier.cpp
@@ -0,0 +1,71 @@
+// scopeIdentifier.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+const double RATE = 10.50;
+int z;
+double t;
+
+void one(int x, char y);
+void two(int a, int b, char x);
+void three(int one, double y, int z);
+
+int main()
+{
+    int num, first;
+    double x, y, z;
+    char name, last;
+      *
+      *
+      *
+    return 0;
+}
+
+void one(int x, char y)
+{
+      *
+      *
+      *
+}
+int w;
+
+void two(int a, int b, char x)
+{
+    int count;
+      *
+      *
+      *
+}
+
+void three(int one, double y, int z)
+{
+    char ch;
+    int a;
+      *
+      *
+      *
+    //Block four
+    {
+        int x;
+        char a;
+           *
+           *
+    }//end Block four
+       *
+       *
+       *
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
