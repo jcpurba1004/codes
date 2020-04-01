d8eb0417df0fc900792d69f42dba4bb3b27ed129
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-04-01 09:49:08 -0800

    Add averageTestScore example.
    Add averageTestScore example.

--- cpp/visualStudio/averageTestScore/averageTestScore/averageTestScore.cpp
+++ cpp/visualStudio/averageTestScore/averageTestScore/averageTestScore.cpp
@@ -1,60 +1,49 @@
 // averageTestScore.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-//Program to calculate the average test score.
+//Program to find the average test score and output the
+//average test score and all the test scores tha are
+//less than the average test score.
 
 #include <iostream>
-#include <fstream>
 #include <iomanip>
-#include <string>
-
+#include "averageTestScore.h"
 using namespace std;
 
 int main()
 {
-    ifstream inFile;   //input file stream variable
-    ofstream outFile;  //output file stream variable
-
-    double test1, test2, test3, test4, test5;
+    int test0, test1, test2, test3, test4;
     double average;
 
-    string firstName;
-    string lastName;
+    cout << fixed << showpoint << setprecision(2);
 
-    inFile.open("test.txt");  //open the input file
+    cout << "Enter five test scores: ";
+    cin >> test0 >> test1 >> test2 >> test3 >> test4;
+    cout << endl;
 
-    if (!inFile)
-    {
-        cout << "Cannot open the input file. "
-             << "The program terminates." << endl;
-        return 1;
-    }
+    average = (test0 + test1 + test2 + test3 + test4) / 5.0;
 
-    outFile.open("testavg.out");  //open the output file
+    cout << "The average test score = " << average << endl;
 
-    outFile << fixed << showpoint;
-    outFile << setprecision(2);
+    if (test0 < average)
+        cout << test0 << " is less than the average "
+             << "test score." << endl;
 
-    cout << "Processing data" << endl;
-    
-    inFile >> firstName >> lastName;
-    outFile << "Student name: " << firstName
-            << " " << lastName << endl;
+    if (test1 < average)
+        cout << test1 << " is less than the average "
+             << "test score." << endl;
 
-    inFile >> test1 >> test2 >> test3
-           >> test4 >> test5;
-    outFile << "Test scores: " << setw(4) << test1
-            << setw(4) << test2 << setw(4) << test3
-            << setw(4) << test4 << setw(4) << test5
-            << endl;
+    if (test2 < average)
+        cout << test2 << " is less than the average "
+             << "test score." << endl;
 
-    average = (test1 + test2 + test3 + test4 + test5) / 5.0;
+    if (test3 < average)
+        cout << test3 << " is less than the average "
+             << "test score." << endl;
 
-    outFile << "Average tet score: " << setw(6)
-            << average << endl;
-
-    inFile.close();
-    outFile.close();
+    if (test4 < average)
+        cout << test4 << " is less than the average "
+             << "test score." << endl;
 
     return 0;
 }
