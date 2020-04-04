5946678a8b4793a079fd492e40849c95b611e897
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-04-04 14:00:48 -0800

    Update switch example.
    Update switch example.

--- cpp/visualStudio/operations/switch/switch.cpp
+++ cpp/visualStudio/operations/switch/switch.cpp
@@ -1,38 +1,50 @@
 // switch.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+//Grade program with bugs.
+
 #include <iostream>
-#include <cmath>
+#include "switch.h"
+
 using namespace std;
 
 int main()
 {
-    
-    int age = 18;
+    int testScore;
 
-    switch (age) {
-        case 16:
-            cout << "hey you can drive now!" << endl;
-            break;
-        case 18:
-            cout << "go buy some lotto tickets~!" << endl;
-            break;
-        case 21:
-            cout << "buy me some beer" << endl;
-            break;
-        default:
-            cout << "sorry you get nothing" << endl;
+    cout << "Enter the test score: ";
+    cin >> testScore;
+    cout << endl;
+    
+    cout << testScore / 10 << endl;
+
+    switch (testScore / 10) 
+    {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+        cout << "The grade is F." << endl;
+        break;
+    case 6:
+        cout << "The grade is D." << endl;
+        break;
+    case 7:
+        cout << "The grade is C." << endl;
+        break;
+    case 8:
+        cout << "The grade is B." << endl;
+        break;
+    case 9:
+    case 10:
+        cout << "The grade is A." << endl;
+        break;
+    default:
+        cout << "Invalid test score." << endl;
+        break;
     }
 
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
