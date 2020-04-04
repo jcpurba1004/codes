0875610db23e909deb662fa02a65e98421a5eff3
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-04-04 14:59:56 -0800

    Update cableCompanyBilling example.
    Update cableCompanyBilling example.

--- cpp/visualStudio/unit6-7/cableCompanyBilling/cableCompanyBilling.cpp
+++ cpp/visualStudio/unit6-7/cableCompanyBilling/cableCompanyBilling.cpp
@@ -12,6 +12,7 @@
 
 #include <iostream>
 #include <iomanip>
+
 using namespace std;
 
       //Named constants - residential customers
@@ -21,26 +22,32 @@ const double RES_COST_PREM_CHANNEL = 7.50;
 
       //Named constants - business customers
 const double BUS_BILL_PROC_FEES = 15.00;
-const double RES_BASIC_SERV_COST = 75.00;
+const double BUS_BASIC_SERV_COST = 75.00;
 const double BUS_BASIC_CONN_COST = 5.00;
 const double BUS_COST_PREM_CHANNEL = 50.00;
 
-double residential();  //Function prototype
-double business();     //Function prototype
-
 int main()
 {
-        //declare variables
+        //Variable declaration
     int accountNumber;
     char customerType;
+    int numOfPremChannels;
+    int numOfBasicServConn;
     double amountDue;
 
     cout << fixed << showpoint;
     cout << setprecision(2);
 
-    cout << "This program computes a cable bill."
-         << endl;
-    cout << "Enter account number: ";
+    cout << "This program computes a cable "
+         << "bill." << endl;
+
+    cout << "Enter account number (an integer): ";
+    cin >> accountNumber;
+    cout << endl;
+
+    cout << "Enter customer type: "
+         << "R or r (Residential), "
+         << "B or b (Business):  ";
     cin >> customerType;
     cout << endl;
 
@@ -48,36 +55,59 @@ int main()
     {
     case 'r':
     case 'R':
-        amountDue = residential();
-        cout << "Account number = "
-             << accountNumber << endl;
-        cout << "Amount due = $"
-             << amountDue << endl;
+        cout << "Enter the number"
+             << " of premium channels: ";
+        cin >> numOfPremChannels;
+        cout << endl;
+ 
+        amountDue = RES_BILL_PROC_FEES
+                   + RES_BASIC_SERV_COST
+                   + numOfPremChannels *
+                     RES_COST_PREM_CHANNEL;
+
+        cout << "Account number: "
+             << accountNumber
+             << endl;
+        cout << "Amount due: $"
+             << amountDue
+             << endl;
         break;
+
     case 'b':
     case 'B':
-        amountDue =business();
-        cout << "Account number = "
-            << accountNumber << endl;
-        cout << "Amount due = $"
-            << amountDue << endl;
+        cout << "Enter the number of basic "
+             << "service connections: ";
+        cin >> numOfBasicServConn;
+        cout << endl;
+
+        cout << "Enter the number"
+             << " of premium channels: ";
+        cin >> numOfPremChannels;
+        cout << endl;
+
+        if (numOfBasicServConn <= 10)
+            amountDue = BUS_BILL_PROC_FEES
+                        + BUS_BASIC_SERV_COST
+                        + numOfPremChannels *
+                          BUS_COST_PREM_CHANNEL;
+        else
+            amountDue = BUS_BILL_PROC_FEES
+                        + BUS_BASIC_SERV_COST
+                        + (numOfBasicServConn - 10) *
+                           BUS_BASIC_CONN_COST
+                        + numOfPremChannels *
+                          BUS_COST_PREM_CHANNEL;
+
+        cout << "Account number: "
+             << accountNumber << endl;
+        cout << "Amount due: $" << amountDue
+             << endl;
         break;
+
     default:
         cout << "Invalid customer type." << endl;
-    }
+    }//end switch
 
     return 0;
 }
 
-//Place the definitions of the functions residential and business here.
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
