b5fb79b68a0d098dffa2820f329227ed92179c1e
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-04-06 15:51:14 -0800

    Create unit8-11 example.
    Create unit8-11 example.

--- cpp/visualStudio/unit8-11/unit8-11/unit8-11.cpp
+++ cpp/visualStudio/unit8-11/unit8-11/unit8-11.cpp
@@ -10,14 +10,31 @@ using namespace std;
 const int NUMBER_OF_ROWS = 6;
 const int NUMBER_OF_COLUMNS = 5;
 
+int main();
+
 void printMatrix(int matrix[][NUMBER_OF_COLUMNS], int  NUMBER_OF_ROWS);
 void sumRows(int matrix[][NUMBER_OF_COLUMNS], int  NUMBER_OF_ROWS);
 void largestInRows(int matrix[][NUMBER_OF_COLUMNS], int  NUMBER_OF_ROWS);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int board[NUMBER_OF_ROWS] [NUMBER_OF_COLUMNS]
+                        = {{17, 8, 24, 10, 28},
+                           {9, 20, 16, 55, 90},
+                           {25, 45, 35, 8, 78},
+                           {5, 0, 96, 45, 38},
+                           {76, 30, 8, 14, 28},
+                           {9, 60, 55, 62, 10}},
+    printMatrix(board, NUMBER_OF_ROWS);
+    cout << endl;
+    sumRows(board, NUMBER_OF_ROWS);
+    cout << endl;
+    largestInRows(board, NUMBER_OF_ROWS);
+
+    return 0;
 }
+//Place the definitions of the functions printMatrix,
+//sumRows, and largestInRows as described previously here.s
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
