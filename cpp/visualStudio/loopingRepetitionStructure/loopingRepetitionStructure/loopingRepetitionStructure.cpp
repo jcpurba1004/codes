23331a4baf49184c9052dff7d83550b94d92cd91
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-04-08 09:51:47 -0800

    Update textProcessing example.
    Update textProcessing example.

--- cpp/visualStudio/loopingRepetitionStructure/loopingRepetitionStructure/loopingRepetitionStructure.cpp
+++ cpp/visualStudio/loopingRepetitionStructure/loopingRepetitionStructure/loopingRepetitionStructure.cpp
@@ -7,25 +7,44 @@ using namespace std;
 
 int main()
 {
-    int calBurnedInADay;
+    int calBurnedDay1, calBurnedDay2, calBurnedDay3,
+        calBurnedDay4, calBurnedDay5, calBurnedDay6,
+        calBurnedDay7;
     int calBurnedInAWeek;
-    int day;
+    
+    cout << "Enter calories burned day 1: ";
+    cin >> calBurnedDay1;
+    cout << endl;
 
-    day = 1;
-    calBurnedInAWeek = 0;
+    cout << "Enter calories burned day 2: ";
+    cin >> calBurnedDay2;
+    cout << endl;
 
-    while (day <= 7)
-    {
-        cout << "Enter calories burned day " << day << ": ";
-        cin >> calBurnedInADay;
-        cout << endl;
+    cout << "Enter calories burned day 3: ";
+    cin >> calBurnedDay3;
+    cout << endl;
 
-        calBurnedInAWeek = calBurnedInAWeek + calBurnedInADay;
-        day = day + 1;
-    }
+    cout << "Enter calories burned day 4: ";
+    cin >> calBurnedDay4;
+    cout << endl;
+
+    cout << "Enter calories burned day 5: ";
+    cin >> calBurnedDay5;
+    cout << endl;
+
+    cout << "Enter calories burned day 6: ";
+    cin >> calBurnedDay6;
+    cout << endl;
+    cout << "Enter calories burned day 7: ";
+    cin >> calBurnedDay7;
+    cout << endl;
+
+    calBurnedInAWeek = calBurnedDay1 + calBurnedDay2 + calBurnedDay3
+                     + calBurnedDay4 + calBurnedDay5 + calBurnedDay6
+                     + calBurnedDay7;
 
     cout << "Average number of calories burned each day: "
-        << calBurnedInAWeek / 7 << endl;
+         << calBurnedInAWeek / 7 << endl;
 
     return 0;
 }
---
+++ cpp/visualStudio/textProcessing/textProcessing/textProcessing.cpp
@@ -0,0 +1,134 @@
+// textProcessing.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+//***************************************************************
+// Author: Jeremiah Purba
+//
+// Program: Line and Letter Count
+// This program reads a text, outputs the text as is, and also
+// prints the number of lines and the number of times each
+// letter appears in the text. An uppercase letter and a
+// lowercase letter are treated as being the same; that is,
+// they are tallied together.
+//***************************************************************
+
+#include <iostream>
+#include <fstream>
+#include <cctype>
+
+using namespace std;
+
+void initialize(int& lc, int list[]);
+void copyText(ifstream& intext, ofstream& outtext, char& ch, int list[]);
+void characterCount(char ch, int list[]);
+void writeTotal(ofstream& outtext, int lc, int list[]);
+
+int main()
+{
+        //Step 1; Declare variables
+    int lineCount;
+    int letterCount[26];
+    char ch;
+    ifstream infile;
+    ofstream outfile;
+
+    infile.open("textin.txt");
+
+    if (!infile)
+    {
+        cout << "Cannot open the input file."
+             << endl;
+        return 1;
+    }
+
+    outfile.open("text.out");
+
+    initialize(lineCount, letterCount);
+
+    infile.get(ch);
+
+    while (infile)
+    {
+        copyText(infile, outfile, ch, letterCount);
+        lineCount++;
+        infile.get(ch);
+    }
+
+    writeTotal(outfile, lineCount, letterCount);
+
+    infile.close();
+    outfile.close();
+
+    return 0;
+}
+
+void initialize(int& lc, int list[])
+{
+    lc = 0;
+
+    for (int j = 0; j < 26; j++)
+        list[j] = 0;
+} //end initialize
+
+void copyText(ifstream& intext, ofstream& outtext, char& ch, int list[])
+{
+    while (ch != '\n')      //process the entire line
+    {
+        outtext << ch;      //output the character
+
+        characterCount(ch, list);   //call the function
+                                    //character count
+        intext.get(ch);     //read the next character
+    }
+    outtext << ch;          //output the newline character
+} //end copyText
+
+void characterCount(char ch, int list[])
+{
+    int index;
+
+    ch = toupper(ch);
+
+    index = static_cast<int>(ch)
+            - static_cast<int>('A');
+
+    if (0 <= index && index < 26)
+        list[index]++;
+} //end characterCount
+
+void writeTotal(ofstream& outtext, int lc, int list[])
+{
+    outtext << endl;
+    outtext << "The number of lines = " << lc << endl;
+
+    for (int index = 0; index < 26; index++)
+        outtext << static_cast<char>(index + static_cast<int>('A'))
+                << " count = "
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+
+void initialize(int& lc, int list[])
+{
+}
+
+void copyText(ifstream& intext, ofstream& outtext, char& ch, int list[])
+{
+}
+
+void characterCount(char ch, int list[])
+{
+}
+
+void writeTotal(ofstream& outtext, int lc, int list[])
+{
+}
