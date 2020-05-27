659a2d66b16b81b67c2ea183a9adbd9fbb17b363
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-05-27 09:26:09 -0800

    Create unit10-7.cpp example.
    Create unit10-7.cpp example.

---
+++ cpp/visualStudio/unit10-7/unit10-7/unit10-7.cpp
@@ -0,0 +1,31 @@
+// unit10-7.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+dataTypeName
+listType
+domain
+Every listType value is an array of, say 1000 numbers
+operations
+Check to see whether the list is empty.
+Check to see whether the list is full.
+Search the list for a given item.
+Delete an item from the list.
+Insert an item in the list.
+Sort the list.
+Destroy the list.
+Print the list.
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
