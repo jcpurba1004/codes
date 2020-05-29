b18ae8133b211bf6a91a0e93f89cbea3bc1a5b77
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-05-29 15:04:34 -0800

    Create unit11-4.cpp example.
    Create unit11-4.cpp example.

---
+++ cpp/visualStudio/unit11-4/unit11-4/unit11-4.cpp
@@ -0,0 +1,34 @@
+// unit11-4.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "primeFactorization.h"
+
+int main()
+{
+    primeFactorization number;
+
+    long long num;
+
+    cout << "Enter an integer between 2 and "
+         << "270,000,000,000,000: ";
+    cin >> num;
+    cout << endl;
+
+    number.setNum(num);
+
+    number.factorization();
+
+    return 0;
+} //end main
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
