bb65a946d6426695e64ef2d7c26e9006af750447
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-06-04 14:36:01 -0800

    Create unit14-18.cpp example.
    Create unit14-18.cpp example.

---
+++ cpp/visualStudio/unit14-18/unit14-18/unit14-18.cpp
@@ -0,0 +1,54 @@
+// unit14-18.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Processing exceptions thrown by a function in the calling
+// environment.
+
+#include <iostream>
+#include "myException.h"
+
+using namespace std;
+
+void functionA() throw (myException);
+void functionB() throw (myException);
+void functionC() throw (myException);
+
+int main()
+{
+    try
+    {
+        functionA();
+    }
+    catch (myException me)
+    {
+        cout << me.what() << " Caught in main." << endl;
+    }
+
+    return 0;
+}
+
+void functionA() throw (myException)
+{
+    functionB();
+}
+
+void functionB() throw (myException)
+{
+    functionC();
+}
+
+void functionC() throw (myException)
+{
+    throw myException("Exception generated in function C.");
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
