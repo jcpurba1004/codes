157f52218fa859fd204d013119b21d5b01a47cec
jcpurba1004 <54612729+jcpurba1004@users.noreply.github.com>
2020-06-04 13:28:57 -0800

    Create unit14-12.cpp example.
    Create unit14-12.cpp example.

---
+++ cpp/visualStudio/unit14-12/unit14-12/unit14-12.cpp
@@ -0,0 +1,52 @@
+// unit14-12.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// Using user-defined exception class divisionByZero with a
+// specific error message.
+
+#include <iostream>
+#include "divisionByZero.h"
+
+using namespace std;
+
+int main()
+{
+    int dividend, divisor, quotient;
+
+    try
+    {
+        cout << "Line 9: Enter the dividend: ";
+        cin >> dividend;
+        cout << endl;
+
+        cout << "Line 12: Enter the divisor: ";
+        cin >> divisor;
+        cout << endl;
+
+        if (divisor == 0)
+            throw divisionByZero("Found division by 0");
+
+        quotient = dividend / divisor;
+        cout << "Line 18: Quotient = " << quotient
+             << endl;
+    }
+    catch (divisionByZero divByZeroObj)
+    {
+        cout << "Line 22: In the divisionByZero "
+             << "catch block: "
+             << divByZeroObj.what() >> endl;
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
