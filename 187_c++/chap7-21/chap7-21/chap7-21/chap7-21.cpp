33d329dabbde553fc255959f10adecb7865329f1
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-06-12 16:09:15 -0700

    Create chap7-21.cpp
    Create chap7-21.cpp

---
+++ 187_c++/chap7-21/chap7-21/chap7-21/chap7-21.cpp
@@ -0,0 +1,56 @@
+// chap7-21.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// This program demonstrates a two-dimensional array.
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    const int NUM_DIVS = 3;            // Number of divisions
+    const int NUM_QTRS = 4;            // Number of quarters
+    double sales[NUM_DIVS] [NUM_QTRS]; // Array with 3 rows and 4 columns.
+    double totalSales = 0;             // To hold the total sales.
+    int div, qtr;                      // Loop counters.
+
+    cout << "This program will calculate the total sales of\n";
+    cout << "all the company's divisions.\n";
+    cout << "Enter the following sales information:\n\n";
+
+    // Nested loops to fill the array with quarterly
+    // sales figures for each division.
+    for (div = 0; div < NUM_DIVS; div++)
+    {
+        for (qtr = 0; qtr < NUM_QTRS; qtr++)
+        {
+            cout << "Division " << (div + 1);
+            cout << ", Quarter " << (qtr + 1) << ": $";
+            cin >> sales[div][qtr];
+        }
+        cout << endl; // Print blank line.
+    }
+
+    // Nested loops used to add all the elements.
+    for (div = 0; div < NUM_DIVS; div++)
+    {
+        for (qtr = 0; qtr < NUM_QTRS; qtr++)
+            totalSales += sales[div][qtr];
+    }
+
+    cout << fixed  << showpoint << setprecision(2);
+    cout << "The total sales for the company are: $";
+    cout << totalSales << endl;
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
