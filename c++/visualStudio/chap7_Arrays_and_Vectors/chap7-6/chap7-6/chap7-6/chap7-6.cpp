92b7990020ad4fc243f0527f973c0dc8288ec680
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-06-17 11:34:30 -0700

    Create chap7-6.cpp
    Create chap7-6.cpp

---
+++ c++/visualStudio/chap7_Arrays_and_Vectors/chap7-6/chap7-6/chap7-6/chap7-6.cpp
@@ -0,0 +1,30 @@
+// chap7-6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// This program has a partially initialized array.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int SIZE = 7;
+    int numbers[SIZE] = {1, 2, 4, 8}; // Initialize first 4 elements
+
+    cout << "Here are the contents of the array:\n";
+    for (int index = 0; index < SIZE; index++)
+       cout << numbers[index] << " ";
+
+    cout << endl;
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
