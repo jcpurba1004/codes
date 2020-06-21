560a4728e9959b1e1075c555b6861fd1a548de3a
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-06-20 22:43:44 -0700

    Create chap7-22.cpp
    Create chap7-22.cpp

--- c++/visualStudio/chap7_Arrays_and_Vectors/chap7-22/chap7-22/chap7-22/chap7-22.cpp
+++ c++/visualStudio/chap7_Arrays_and_Vectors/chap7-22/chap7-22/chap7-22/chap7-22.cpp
@@ -4,25 +4,25 @@
 // This program demonstrates accepting a 2D array argument.
 #include <iostream>
 #include <iomanip>
+#include "chap7-22.h"
 using namespace std;
 
-// Global constants
+// Global constants 
 const int COLS = 4;      // Number of columns in each array
 const int TBL1_ROWS = 3; // Number of rows in table1
-const int TBL2_ROWS = 4; // Number of rows in table2
+const int TBL1_ROWS = 4; // Number of rows in table2
 
-void showArray(const int [] [COLS], int); // Function prototype
+void showArray(const int [][COLS], int); // Function prototype
 
 int main()
 {
-    int table1 [TBL1_ROWS] [COLS] = {{1, 2, 3, 4},
-                                     {5, 6, 7, 8},
-                                     {9, 10, 11, 12}};
-    int table2[TBL2_ROWS][COLS] =   {{10, 20, 30, 40},
-                                     {50, 60, 70, 80},
-                                     {90, 100, 110, 120}, 
-                                     {130, 140, 150, 160}};
-
+    int table1[TBL1_ROWS][COLS] = {{1, 2, 3, 4},
+                                   {5, 6, 7, 8},
+                                   {9, 10, 11, 12}};
+    int table2[TBL2_ROWS][COLS] = {{10, 20, 30, 40},
+                                   {50, 60, 70, 80},
+                                   {90, 100, 110, 120},
+                                   {130, 140, 150, 160}};
 
     cout << "The contents of table1 are:\n";
     showArray(table1, TBL1_ROWS);
@@ -31,12 +31,12 @@ int main()
     return 0;
 }
 
-//******************************************************************
-// Function Definition for showArray                               *
-// The first argument is a two-dimensional int array with COLS     *
-// columns. The second argument, rows, specifies the the number of *
-// rows in the array. The function displays the array's contents.  *
-//******************************************************************
+//*****************************************************************
+// Function Definition for showArray                              *
+// The first argumentis a two-dimensional int array with COLS     *
+// columns. The second argument, rows, specifies the number of    *
+// rows in the array. The function displays the array's contents. *
+//*****************************************************************
 
 void showArray(const int numbers[][COLS], int rows)
 {
