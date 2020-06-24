53535247e803a34bd0ab7234601f0ce100fb9543
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-06-23 19:41:27 -0700

    Create Strings.java
    Create Strings.java

---
+++ java/cisc_190/eclipse/Strings/Strings/src/Strings.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+
+/**
+ * @author jpurba
+ *
+ */
+public class Strings {
+
+	/**
+	 * 
+	 */
+	public Strings() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name1 = "George";
+		String name2 = "Franklin";
+		String name3 = "Jay";
+		String name4 = "Ozzy";
+		String name5 = "Carmine";
+		String name6 = "Dee";
+		System.out.printf("%10s%10s\n", name1, name2);
+		System.out.printf("%10s%10s\n", name3, name4);
+		System.out.printf("%10s%10s\n", name5, name6);
+	}
+
+}
