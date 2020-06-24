efcef607d66857bb086cde8e732f00afca76f606
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-06-23 19:57:24 -0700

    Update Strings.java
    Update Strings.java

--- java/cisc_190/eclipse/Strings/Strings/src/Strings.java
+++ java/cisc_190/eclipse/Strings/Strings/src/Strings.java
@@ -29,6 +29,12 @@ public class Strings {
 		System.out.printf("%10s%10s\n", name1, name2);
 		System.out.printf("%10s%10s\n", name3, name4);
 		System.out.printf("%10s%10s\n", name5, name6);
+		
+		int hours = 40;
+		double pay = hours * 25;
+		String name = "Jay";
+		System.out.printf("Name: %s, Hours: %d, Pay: $%,.2f\n",
+				          name, hours, pay);
 	}
 
 }
