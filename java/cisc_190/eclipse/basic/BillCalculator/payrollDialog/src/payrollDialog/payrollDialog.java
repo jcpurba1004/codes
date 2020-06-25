b6630ff65721320ea925ac1020108db1242ccc4a
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-06-25 11:56:31 -0700

    Create payrollDialog.java
    Create payrollDialog.java

--- java/cisc_190/eclipse/basic/BillCalculator/payrollDialog/src/payrollDialog/payrollDialog.java
+++ java/cisc_190/eclipse/basic/BillCalculator/payrollDialog/src/payrollDialog/payrollDialog.java
@@ -1,14 +1,25 @@
 /**
+ * Lead Author(s):
+ * Jeremiah Purba; 5550010017
  * 
+ * Other Contributors:
+ * None
+ * 
+ * References:
+ * Gaddis, T. (2015). Starting Out With Java Myprogramming Lab 
+ * From Control Structures Through Objects. (6th ed.). Addison-Wesley.
+ * 
+ * Version: 1.0 (06/25/2020)
  */
 package payrollDialog;
 
 /**
- * @author Dell
+ * @author jpurba
  *
  */
 
 import javax.swing.JOptionPane;
+
 
 public class payrollDialog {
 
@@ -21,12 +32,19 @@ public class payrollDialog {
 		String name;            // The user's name
 		int hours;              // The number of hours worked
 		double payRate;         // The user's hourly pay rate
+		double grossPay;        // The user's gross pay
 		
 		// Get the user's name
 		name = JOptionPane.showInputDialog("What is " + "your name? ");
 		
 		// Get the hours worked
 		inputString = JOptionPane.showInputDialog("How many hours " + "did you work this week? ");
+		
+		// Convert the input to an int
+		hours = Integer.parseInt(inputString);
+		
+		// Get the hourly pay rate
+		inputString = JOptionPane.showInputDialog("What is your " + "hourly pay rate? ");
 		
 		// Convert the input to a double
 		payRate = Double.parseDouble(inputString);
