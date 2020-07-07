a65e64ba2ba23a07ada798aa0e51dad575254892
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-07-07 15:13:07 -0700

    Create chap18-2.cpp
    Create chap18-2.cpp

---
+++ c++/visualStudio/chap18_Linked_Lists/chap18-2/chap18-2/chap18-2/chap18-2.cpp
@@ -0,0 +1,34 @@
+// chap18-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// This program demonstrates the displayList member function.
+#include <iostream>
+#include "NumberList.h"
+#include "chap18-2.h"
+using namespace std;
+
+int main()
+{
+    // Define a NumberList object.
+    NumberList list;
+
+    // Append some values to the list.
+    list.appendNode(2.5);
+    list.appendNode(7.9);
+    list.appendNode(12.6);
+
+    // Display the values in the list.
+    list.displayList();
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
