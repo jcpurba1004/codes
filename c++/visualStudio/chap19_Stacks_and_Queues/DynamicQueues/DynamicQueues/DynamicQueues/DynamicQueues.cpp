24d0720e847928bafd0a4a6d16597e6cdc5d8d67
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-07-15 21:31:24 -0700

    Create DynamicQueues.cpp and DynIntQueue.cpp
    Create DynamicQueues.cpp and DynIntQueue.cpp

--- c++/visualStudio/chap19_Stacks_and_Queues/DynamicQueues/DynamicQueues/DynamicQueues/DynamicQueues.cpp
+++ c++/visualStudio/chap19_Stacks_and_Queues/DynamicQueues/DynamicQueues/DynamicQueues/DynamicQueues.cpp
@@ -1,20 +1,30 @@
 // DynamicQueues.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+// This program demonstrates the DynIntQueue class.
 #include <iostream>
+#include "DynIntQueue.h"
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int MAX_VALUES = 5;
+
+    // Create a DynIntQueue object.
+    DynIntQueue iQueue;
+
+    // Enqueue a series of numbers.
+    cout << "Enqueuing " << MAX_VALUES << " items...\n";
+    for (int x = 0; x < 5; x++)
+        iQueue.enqueue(x);
+
+    // Dequeue and retrieve all numbers in the queue
+    cout << "The values in the queue were:\n";
+    while (!iQueue.isEmpty())
+    {
+        int value;
+        iQueue.dequeue(value);
+        cout << value << endl;
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
