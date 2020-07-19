a801cee602117a0abbecdc08c742cf6a03a67f00
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-07-19 14:46:17 -0700

    Create chap20-2.cpp
    Create chap20-2.cpp

---
+++ c++/visualStudio/chap20_Recursion/chap20-2/chap20-2/chap20-2/chap20-2.cpp
@@ -0,0 +1,46 @@
+// chap20-2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+// This program demonstrates a simple recursive function.
+#include <iostream>
+using namespace std;
+
+// Function prototype
+void message(int);
+
+int main()
+{
+    message(5);
+    return 0;
+}
+
+//************************************************************
+// Definition of function message. If the value in times is  *
+// greater than 0, the message is displayed and the function *
+// is recursively called with the argument times - 1.        *                                               *
+//************************************************************
+
+void message(int times)
+{
+    cout << "message called with " << times << " in times.\n";
+
+    if (times > 0)
+    {
+        cout << "This is a recursive function.\n";
+        message(times - 1);
+    }
+
+    cout << "message returning with " << times;
+    cout << " in times.\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
