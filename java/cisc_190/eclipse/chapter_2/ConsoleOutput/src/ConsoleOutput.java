321756ebdc4ab71d21c5347e7075bdae3130aa90
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-12-15 17:11:34 -0800

    Create Simple.java and ConsoleOutput.java example.
    Create Simple.java and ConsoleOutput.java example.

---
+++ java/cisc_190/eclipse/chapter_2/ConsoleOutput/src/ConsoleOutput.java
@@ -0,0 +1,11 @@
+// This program demonstrates console output.
+
+public class ConsoleOutput 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("One");
+		System.out.println("Two");
+		System.out.println("Three");
+	}
+}
