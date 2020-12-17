0804ffe4a3b6474faede104e23156e92cbe243e5
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2020-12-16 17:40:04 -0800

    Create VariableDemo.java, MathDemo.java and MilesPerGallon.java example.
    Create VariableDemo.java, MathDemo.java and MilesPerGallon.java example.

---
+++ java/cisc_190/eclipse/chapter_2/MilesPerGallon/src/MilesPerGallon.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class MilesPerGallon
+{
+	public static void main(String[] args)
+    {
+		double miles;       // Miles driven
+		double gallons;     // Gallons used
+		double mpg;         // Miles-per-gallon
+		
+		// Create a Scanner object for keyboard input.
+		Scanner keyboard = new Scanner(System.in);
+		
+		// Prompt the user to enter miles driven.
+		System.out.print("Enter the miles driven: ");
+		miles = keyboard.nextDouble();
+		
+		// Prompt the user to enter the gallons used.
+		System.out.print("Enter the gallons used: ");
+		gallons = keyboard.nextDouble();
+		
+		// Calculate miles-per-gallon.
+		mpg = miles / gallons;
+		
+		// Display the miles-per-gallon.
+		System.out.println("The MPG is " + mpg);
+	}
+}
---
+++ java/cisc_190/eclipse/chapter_2/VariableDemo/src/VariableDemo.java
@@ -0,0 +1,10 @@
+public class VariableDemo 
+{
+	public static void main(String[] args) 
+	{
+		double balance;
+		
+		balance = 1234.56;
+		System.out.println(balance);
+	}
+}
