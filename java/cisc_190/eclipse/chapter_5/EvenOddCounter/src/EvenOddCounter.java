e99fd0dadca0159d8f3dfcaa508088e294ca223d
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-01-15 11:17:38 -0800

    Create EvenOddCounter.java example.
    Create EvenOddCounter.java example.

---
+++ java/cisc_190/eclipse/chapter_5/EvenOddCounter/src/EvenOddCounter.java
@@ -0,0 +1,47 @@
+/*
+Problem 5.15: Even/Odd Counter
+
+You can use the following logic to determine whether a number is even or odd:
+
+if ((number % 2) == 0)
+{
+// The number is even.
+}
+else
+{
+// The number is odd.
+}
+
+Write a program with a method named isEven that accepts an int argument.
+*/
+
+import java.util.Random;
+
+public class EvenOddCounter{
+
+	public static boolean isEven( int number ) {
+		if( ( number % 2 ) == 0 ) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public static void main(String[ ] args) {
+		Random random = new Random();
+		int randomNumberGenerated;
+		int numberOfEvenNumbers = 0;
+		int numberOfOddNumbers = 0;
+		
+		for( int currentNumber = 1; currentNumber <= 100; currentNumber++ ) {
+			randomNumberGenerated = random.nextInt( 10 ) + 1;
+			if( isEven( randomNumberGenerated ) ) {
+				numberOfEvenNumbers += 1;
+			} else {
+				numberOfOddNumbers += 1;
+			}
+		}
+		System.out.println( "There were " + numberOfEvenNumbers + " even numbers and " + numberOfOddNumbers + " odd numbers" );
+	}
+	
+}
--- java/cisc_190/eclipse/chapter_5/PaintJob/src/PaintJob.java
+++ java/cisc_190/eclipse/chapter_5/PaintJob/src/PaintJob.java
@@ -38,18 +38,72 @@ public class JobEstimator{
 	}
 	
 	public static double getTotalWallSpace( int numberOfRooms ) {
+		String userInputString;
 		double wallSpace;
 		double totalWallSpace = 0;
 		for( int currentRoom = 1; currentRoom <= numberOfRooms; currentRoom++) {
-			wallSpace = JOptionPane.showInputDialog( "Please enter the square feet of wall space in room " + currentRoom );
-			totalWallSpace += squareFeet;
+			userInputString = JOptionPane.showInputDialog( "Please enter the square feet of wall space in room " + currentRoom);
+			wallSpace = Double.parseDouble( userInputString );
+			totalWallSpace += wallSpace;
 		}
 		return totalWallSpace;
 	}
 	
 	public static int main(String[ ] args) {
-		
-
+		/*
+		If     115 square feet = 1 gallon
+		then   totalWallSpace  =    ?
+		*/
+		double gallonsOfPaintOfRequired;
+		gallonsOfPaintRequired = ( totalWallSpace / 115 ) * 8;
+		return gallonsOfPaintRequired;
 	}
-
+	
+	public static double calculateCostOfPaint( double priceOfPaint, double gallonsOfPaintRequired ) {
+		double costOfPaint;
+		costOfPaint = gallonsOfPaintRequired * priceOfPaint;
+		return costOfPaint;
+	}
+	
+	public static double calculateLaborCharges( double hoursOfLaborRequired, double chargePerHour ) {
+		double laborCharges;
+		laborCharges = hoursOfLaborRequired * chargePerHour;
+		return laborCharges;
+	}
+	
+	public static double calculateTotalCost( double laborCharges, double costOfPaint ) {
+		double totalCost;
+		totalCost = laborCharges * costOfPaint;
+		return totalCost;
+	}
+	
+	public static void displayDataOnScreen( int userNumberRooms, double totalWallSpace, double priceOfPaintPerGallon,
+			                               double gallonsOfPaintRequired, double hoursOfLaborRequired, double costOfPaint,
+			                               double laborCharges, double totalCost ) {
+		String outputString;
+		outputString = String.format( "User number of rooms: %d\nTotal wall space: %.2f\nPrice of paint per gallon:"+
+						             " $%,.2f\nGallons of paint" required: %.1f\nHours of labor required: %.1f\nCost of paint:"+ "
+						             " $%,.2f\nLabor Charges: $%,.2f\nTotal Cost: $%,.2f", userNumberRooms, totalWallSpace, 
+						             priceOfPaintPerGallon, gallonsOfPaintRequired, hoursOfLaborRequired, costOfPaint, 
+						             laborCharges, totalCost );
+		JOptionPane.showMessageDialog( null, outputString );
+	}
+	
+	public static void main( String [ ] args ) {
+		final double CHARGE_PER_HOUR = 18.00;
+		int userNumberOfRooms = getNumberOfRooms();
+		double totalWallSpace = getTotalWallSpace( userNumberOfRooms );
+		double priceOfPaintPerGallon = getPaintPricePerGallon();
+		double gallonsOfPaintRequired = calculateGallonsOfPaintRequired( totalWallSpace );
+		double hoursOfLaborRequired = calculateHoursOfLaborRequired( totalWallSpace );
+		double costOfPaint = calculateCostOfPaint( priceOfPaintPerGallon, gallonsOfPaintRequired );
+		double laborCharges = calculateLaborCharges( hoursOfLaborRequired, CHARGE_PER_HOUR );
+		double totalCost = calculateTotalCost( laborCharges, costOfPaint );
+		displayDataOnScreen( userNumberOfRooms, totalWallSpace, priceOfPaintPerGallon, gallonsOfPaintRequired,
+				            hoursOfLaborRequired, costOfPaint, laborCharges, totalCost );
+		System.exit( 0 );
+		
+	}
+	
+	
 }
