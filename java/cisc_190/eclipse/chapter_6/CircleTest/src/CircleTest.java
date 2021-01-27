555fcc55d8fcc8eb2b257802380c50986b259e78
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-01-26 17:01:08 -0800

    Create TemperatureTest.java example.
    Create TemperatureTest.java example.

--- java/cisc_190/eclipse/chapter_6/CircleTest/src/CircleTest.java
+++ java/cisc_190/eclipse/chapter_6/CircleTest/src/CircleTest.java
@@ -1,9 +1,38 @@
+/*
+
+Problem 6.7: Circle class
+
+Write a Circle class that has the following fields:
+
+* radius: a double
+* PI: a final double initialized with the value 3.14159
+
+Write a program that demonstrates the Circle class by asking the user for the circle's radius, creating a Circle object
+and then reporting the circle's area, diameter and circumference.
+
+*/
+
+import javax.swing.JOptionPane;
 
 public class CircleTest {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String userInputString;
+		double userRadius;
+		String userOutputString;
+		
+		userInputString = JOptionPane.showInputDialog( "Please enter the circle's radius: " );
+		userRadius = Double.parseDouble( userInputString );
+		
+		Circle circle1 = new Circle();
+		
+		circle1.setRadius( userRadius );
+		
+		String.format( "Area: %f\nDiameter: %f\nCircumference: %f", circle1.getArea(), 
+				      circle1.getDiameter(), circle1.getCircumference() );
+		
+		JOptionPane.showMessageDialog( null, userOutputString );
+		
+		System.exit( 0 );
 	}
-
 }
