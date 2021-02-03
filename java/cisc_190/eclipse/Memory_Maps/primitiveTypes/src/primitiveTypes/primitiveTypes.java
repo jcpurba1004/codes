7aa85d92f03550b14146cdd46a69c23ba1cebb29
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-02-03 14:42:44 -0800

    Create primitiveTypes.java example.
    Create primitiveTypes.java example.

---
+++ java/cisc_190/eclipse/Memory_Maps/primitiveTypes/src/primitiveTypes/primitiveTypes.java
@@ -0,0 +1,13 @@
+package primitiveTypes;
+
+public class primitiveTypes 
+{
+	public static void main(String[] args) 
+	{
+		int x = 3;
+		int y = 3;
+		System.out.println(3 == 3);
+		x = 4;
+		System.out.println(3 == 3);
+	}
+}
