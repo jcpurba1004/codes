d15224c2724faa6edc3f2708e25bf3484f1f7431
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-02-03 13:57:24 -0800

    Create MO_SMART_Goals.java example.
    Create MO_SMART_Goals.java example.

---
+++ java/cisc_190/BlueJ/Md_1_Programming_Challenges/SMART_Goals/M0_SMART_Goals.java
@@ -0,0 +1,60 @@
+
+/**
+ * Write a description of class M0_SMART_Goals here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class M0_SMART_Goals
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class M0_SMART_Goals
+     */
+    public M0_SMART_Goals()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * Practice writing a SMART goal related to this class. 
+     * Include the level of proficiency that you want to attain by the end 
+     * of this course. Submit your SMART goal and details for how your goal
+     * is:
+     * 
+     * Specific
+     * Measurable
+     * Attainable
+     * Relevant
+     * Time Bound
+     * 
+     */
+    
+    public int sampleMethod(int y)
+    {
+        * I will set aside time in my calendar app to work on this class;
+        * I will eliminate distractions during study time (turn off internet 
+          not related to the course, turn off TV, put phone away);
+        * I will quiz myself after reading something for class, or practice 
+          a problem demonstrated in the book, just to determine if I "get it"
+          before moving on;
+        * I will create study guides for myself;
+        * I will work with a study group outside of class hours;
+        * I will seek out tutoring;
+        * I will attend office hours;
+        * I will know when I need help and make a plan to get help from the 
+          professor, a tutor or someone I know in the class;
+        * I will ask a question on Pronto or the Q&A Forum when I have felt
+          stuck for 15 minutes; if I figure out the solution I will also post
+          a summary of my solution;
+        * I will answer other students' questions on Pronto or the Q&A Forum;
+        * I will contact the professor after class (either in office hours or
+          via the Canvas Inbox) if I'm confused about something from class;
+        * I will explain my programming projects to my family;
+        * I will turn in work by the deadlines;
+        * I will get enough sleep;
+    }
+}
