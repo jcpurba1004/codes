fb8e3656b6b64cf329f5ccae9acf10508160d597
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-02-24 09:46:50 -0800

    Create Solution.java example.
    Create Solution.java example.

---
+++ java/cisc_190/eclipse/Memory_Maps/memoryMapTypes/src/memoryMapTypes/memoryMapTypes.java
@@ -0,0 +1,13 @@
+package memoryMapTypes;
+
+public class memoryMapTypes 
+{
+	public static void main(String[] args) 
+	{
+		int x = 3;
+		int y = 2;
+		System.out.println(x == 3);
+		int z = x + y;
+		System.out.println(z);
+	}
+}
---
+++ java/cisc_190/eclipse/int_to_String/int_String/src/int_String/Solution.java
@@ -0,0 +1,63 @@
+package int_String;
+import java.util.*;
+import java.security.*;
+public class Solution {
+	public static void main(String[] args) {
+	 
+	 try {
+	  Scanner in = new Scanner(System.in);
+	  int n = in.nextInt();
+	  in.close();
+	  //String s=???; Complete this line below
+	  // There are many ways to solve this question. Here are some.
+	  
+    // 1. Solution
+    String s1 = String.valueOf(n);
+    // 2. Solution
+    String s2 = Integer.toString(n);
+    // 3.Solution
+    // Stack stc = new Stack();
+    // int count=0;
+    // while(n!=0){
+    //	   stc.add(n%10);
+    //         n=n/10;
+    //     count++
+	// }
+    // StringBuilder sb = new StringBuilder();
+    // for(int i=0; i<count; i++){
+    //     sb.append(stc.pop());
+    
+	// }
+    // String s=sb.toString();
+    
+       if (n == Integer.parseInt(s1)) {
+    	System.out.println("Good job");
+       } else {
+    	System.out.println("Wrong answer.");
+       }
+	 }  catch (DoNotTerminate.ExitTrappedException e) {
+	  System.out.println("Unsuccessful Termination!!");
+	 }
+   }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+	
+ public static class ExitTrappedException extends SecurityException {
+  
+  private static final long serialVersionUID = 1;
+ }
+ 
+ public static void forbidExit() {
+  final SecurityManager securityManager = new SecurityManager() {
+   @Override
+   public void checkPermission(Permission permission) {
+	if (permission.getName().contains("exitVM")) {
+	 throw new ExitTrappedException();
+	}
+   }
+  };
+  System.setSecurityManager(securityManager);
+ }
+}
