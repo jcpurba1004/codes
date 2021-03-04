02833eda8fc7a3ff12e97911ce150229552a84a9
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-03-04 10:08:58 -0800

    Create Inheritance_2.java example.
    Create Inheritance_2.java example.

--- java/cisc_190/eclipse/Inheritance/Inheritance_1/src/Inheritance_1.java
+++ java/cisc_190/eclipse/Inheritance/Inheritance_1/src/Inheritance_1.java
@@ -1,9 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
+class Animal{
+		void walk() {
+				System.out.println("I am walking");
+		}
+}
+
+class Bird extends Animal {
+	void fly() {
+		System.out.println("I am flying");
+	}
+	void sing() {
+		System.out.println("I am singing");
+	}
+}
 public class Inheritance_1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+		bird.sing();
 	}
-
 }
