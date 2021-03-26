f2c84970415bcaa554986c0ca61bf278033a3ec3
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-03-26 09:44:12 -0700

    Create DateAndTime.java example.
    Create DateAndTime.java example.

--- java/cisc_190/eclipse/Days/DateAndTime/src/DateAndTime.java
+++ java/cisc_190/eclipse/Days/DateAndTime/src/DateAndTime.java
@@ -1,9 +1,36 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+import javax.naming.spi.DirStateFactory.Result;
+
+import java.text.DateFormatSymbols;
+import java.util.Calendar;
+import java.util.Scanner;
 
 public class DateAndTime {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+		
+		String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+&", "").split(" ");
+		
+		int month = Integer.parseInt(firstMultipleInput[0]);
+		
+		int day = Integer.parseInt(firstMultipleInput[1]);
+		
+		int year = Integer.parseInt(firstMultipleInput[2]);
+		
+		String res = Result.findDay(month, day, year);
+		
+		bufferedWriter.write(res);
+		bufferedWriter.newLine();
+		
+		bufferedReader.close();
+		bufferedWriter.close();
 	}
-
 }
