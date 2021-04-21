ea623912d609cbd5c4238b1f68757668478bc807
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-04-21 10:28:14 -0700

    Create Simple.java example.
    Create Simple.java example.

---
+++ java/cisc_190/eclipse/Programs/JavaProgram1/src/Simple.java
@@ -0,0 +1,10 @@
+//This is a simple Java program
+package javaprogram1;
+public class Simple {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Programming is great fun!");
+	}
+
+}
