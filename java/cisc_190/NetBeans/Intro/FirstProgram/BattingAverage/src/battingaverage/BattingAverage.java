fe1aacadca4faca423c3a891a0e25f5c09ae4c12
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-04-23 10:35:57 -0700

    Create BattingAverage.java example.
    Create BattingAverage.java example.

---
+++ java/cisc_190/NetBeans/Intro/FirstProgram/BattingAverage/src/battingaverage/BattingAverage.java
@@ -0,0 +1,11 @@
+package battingaverage;
+
+public class BattingAverage {
+
+    public static void main(String[] args) {
+        int atBats = 432;
+        int hits = 139;
+        System.out.println("The batting average is:");
+        System.out.println(hits / atBats);
+    }
+}
