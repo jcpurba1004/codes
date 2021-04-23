725193191329cf610ef92b8e69c6ee8ec4726db0
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-04-22 23:07:54 -0700

    Create MainClass.java and MainFrame.java example.
    Create MainClass.java and MainFrame.java example.

---
+++ java/cisc_190/NetBeans/Intro/FirstProgram/MainClass/src/MainClass.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author jpurba
+ */
+public class MainClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello every body");
+        MainFrame myFrame = new MainFrame();
+        myFrame.init();
+    }
+    
+}
