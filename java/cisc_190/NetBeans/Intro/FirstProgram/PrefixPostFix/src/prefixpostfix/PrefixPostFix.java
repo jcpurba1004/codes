52effd768970e378614824c6473e2c9eab9c2469
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-04-26 15:50:42 -0700

    Create PrefixPostFix.java example.
    Create PrefixPostFix.java example.

---
+++ java/cisc_190/NetBeans/Intro/FirstProgram/PrefixPostFix/src/prefixpostfix/PrefixPostFix.java
@@ -0,0 +1,12 @@
+
+package prefixpostfix;
+
+public class PrefixPostFix {
+
+    public static void main(String[] args) {
+        int x = 10;
+        if (++x > 10)
+        System.out.println("x is "
+                + "greater than 10.");
+    }   
+}
