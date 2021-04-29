6492a1de931f5dd82302359294e90d3b9dd57286
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-04-29 16:24:44 -0700

    Create Triangle.java example.
    Create Triangle.java example.

---
+++ java/cisc_190/NetBeans/Intro/FirstProgram/Triangle/src/abstractclasses/Triangle.java
@@ -0,0 +1,28 @@
+package abstractclasses;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author jpurba
+ */
+public class Triangle extends Shape {
+    private double base;
+    private double height;
+    
+    public Triangle(double b, double h) {
+        base = b;
+        height = h;
+    }
+    @Override
+    public double calculateArea() {
+        return base * height / 2.0;
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }   
+}
