b29c8fa215e28926685f76b5658c8ffa0ee6a7d7
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-06-03 16:42:42 -0700

    Create BeautifulQuadruples.java example.
    Create BeautifulQuadruples.java example.

---
+++ java/cisc_190/eclipse/Mathematics/BeautifulQuadruples/src/BeautifulQuadruples.java
@@ -0,0 +1,156 @@
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class BeautifulQuadruples {
+	InputStream is;
+    PrintWriter out;
+    String INPUT = "";
+    
+    void solve()
+    {
+        int[] t = na(4);
+        Arrays.sort(t);
+        long[] dp = new long[3001];
+        dp[1] = 1;
+        for(int u = 0;u < 4;u++){
+            for(int i = 1;i <= t[u];i++){
+                dp[i] += dp[i-1];
+            }
+        }
+        long all = Arrays.stream(dp).sum();
+        
+        int[][] ab = new int[1<<12][3001];
+        for(int i = 1;i <= t[0];i++){
+            for(int j = i;j <= t[1];j++){
+                ab[i^j][j]++;
+            }
+        }
+        
+        int[] abc = new int[1<<12];
+        for(int k = 1;k <= t[2];k++){
+            for(int u = 0;u < 1<<12;u++){
+                abc[u] += ab[u][k];
+            }
+            for(int l = k;l <= t[3];l++){
+                all -= abc[k^l];
+            }
+        }
+        out.println(all);
+    }
+    
+    void run() throws Exception
+    {
+        is = INPUT.isEmpty() ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+        out = new PrintWriter(System.out);
+        
+        long s = System.currentTimeMillis();
+        solve();
+        out.flush();
+        if(!INPUT.isEmpty())tr(System.currentTimeMillis()-s+"ms");
+    }
+    
+    public static void main(String[] args) throws Exception { new D().run(); }
+    
+    private byte[] inbuf = new byte[1024];
+    private int lenbuf = 0, ptrbuf = 0;
+    
+    private int readByte()
+    {
+        if(lenbuf == -1)throw new InputMismatchException();
+        if(ptrbuf >= lenbuf){
+            ptrbuf = 0;
+            try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+            if(lenbuf <= 0)return -1;
+        }
+        return inbuf[ptrbuf++];
+    }
+    
+    private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+    private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+    
+    private double nd() { return Double.parseDouble(ns()); }
+    private char nc() { return (char)skip(); }
+    
+    private String ns()
+    {
+        int b = skip();
+        StringBuilder sb = new StringBuilder();
+        while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+            sb.appendCodePoint(b);
+            b = readByte();
+        }
+        return sb.toString();
+    }
+    
+    private char[] ns(int n)
+    {
+        char[] buf = new char[n];
+        int b = skip(), p = 0;
+        while(p < n && !(isSpaceChar(b))){
+            buf[p++] = (char)b;
+            b = readByte();
+        }
+        return n == p ? buf : Arrays.copyOf(buf, p);
+    }
+    
+    private char[][] nm(int n, int m)
+    {
+        char[][] map = new char[n][];
+        for(int i = 0;i < n;i++)map[i] = ns(m);
+        return map;
+    }
+    
+    private int[] na(int n)
+    {
+        int[] a = new int[n];
+        for(int i = 0;i < n;i++)a[i] = ni();
+        return a;
+    }
+    
+    private int ni()
+    {
+        int num = 0, b;
+        boolean minus = false;
+        while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+        if(b == '-'){
+            minus = true;
+            b = readByte();
+        }
+        
+        while(true){
+            if(b >= '0' && b <= '9'){
+                num = num * 10 + (b - '0');
+            }else{
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+    
+    private long nl()
+    {
+        long num = 0;
+        int b;
+        boolean minus = false;
+        while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+        if(b == '-'){
+            minus = true;
+            b = readByte();
+        }
+        
+        while(true){
+            if(b >= '0' && b <= '9'){
+                num = num * 10 + (b - '0');
+            }else{
+                return minus ? -num : num;
+            }
+            b = readByte();
+        }
+    }
+    
+    private static void tr(Object... o) { System.out.println(Arrays.deepToString(o)); }
+}
