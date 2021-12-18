bfc3d5c8f4b4cc6187625040c1c7a0434e3c8340
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-12-17 17:26:23 -0800

    Create ReactFAST.java example.
    Create ReactFAST.java example.

---
+++ java/cisc_190/eclipse/JavaTutorials/ReactFAST/src/ReactFAST.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ReactFAST {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println("3");
+		Thread.sleep(1000);
+		System.out.println("2");
+		Thread.sleep(1000);
+		System.out.println("1");
+		Thread.sleep(1000);
+		
+		System.out.println("GO!!!!!!!!!");
+		
+		Scanner s = new Scanner(System.in);
+		String a = s.next();
+		System.out.println(a);
+		
+	}
+	
+
+}
