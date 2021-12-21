aef4cf5e8b4c8e02faad6b7997837b260dd1a861
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-12-20 16:09:30 -0800

    Create CapProgram.java example.
    Create CapProgram.java example.

---
+++ java/cisc_190/eclipse/JavaTutorials/CapProgram/src/CapProgram.java
@@ -0,0 +1,11 @@
+
+public class CapProgram {
+
+	public static void main(String[] args) {
+		
+		String s = "happy birthday";
+		String result = s.substring(0, 1).toUpperCase() + s.substring(1);
+		System.out.println(result);
+	}
+
+}
