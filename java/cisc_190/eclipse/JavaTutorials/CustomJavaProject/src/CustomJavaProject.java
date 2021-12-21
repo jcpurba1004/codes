52d4e3e5a73ed3fb6e1f307fd1476f9372d1491f
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-12-20 18:24:21 -0800

    Create WPMProgram.java example.
    Create WPMProgram.java example.

---
+++ java/cisc_190/eclipse/JavaTutorials/CustomJavaProject/src/CustomJavaProject.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CustomJavaProject {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		int d = scan.nextInt();
+		int e = scan.nextInt();
+
+		System.out.println(a + b + c + d + e);
+	}
+}
