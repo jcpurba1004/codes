ea70009a2e8fb2292bd02d3bf41f024d97543c03
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2021-12-20 16:45:43 -0800

    Create Encrypt.java example.
    Create Encrypt.java example.

---
+++ java/cisc_190/eclipse/JavaTutorials/Encrypt/src/Encrypt.java
@@ -0,0 +1,19 @@
+
+public class Encrypt {
+
+	public static void main(String[] args) {
+		
+		int key = 6;
+		
+		String text = "Hey, how's it hangin?";
+		
+		System.out.println(text);
+		
+		char[] chars = text.toCharArray();
+		
+		for(char c : chars) {
+			c += key;
+			System.out.print(c);
+		}	
+	}
+}
