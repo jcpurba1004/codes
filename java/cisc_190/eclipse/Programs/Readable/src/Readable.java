ca3518af8f2a4eaf971dcbaa5a2edfdf10581725
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-01-04 17:06:08 -0800

    Create Readable.java example.
    Create Readable.java example.

--- java/cisc_190/eclipse/Programs/Readable/src/Readable.java
+++ java/cisc_190/eclipse/Programs/Readable/src/Readable.java
@@ -1,9 +1,16 @@
+/**
+This example is much more readable than Compact.java.
+*/
 
-public class Readable {
+ public class Readable
+ {
+	 public static void main(String[] args)
+	 {
+		 int shares = 220;
+		 double averagePrice = 14.67;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		 System.out.println("There were " + shares +
+				 " shares sold at $" +
+				 averagePrice + " per share.");
+	 }
 }
