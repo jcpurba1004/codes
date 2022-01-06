be7b042b3421752e43d48586345196485d9f031d
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-01-06 15:54:47 -0800

    Create CurrencyFormat.java example.
    Create CurrencyFormat.java example.

--- java/cisc_190/eclipse/Strings/CurrencyFormat/src/CurrencyFormat.java
+++ java/cisc_190/eclipse/Strings/CurrencyFormat/src/CurrencyFormat.java
@@ -1,33 +1,14 @@
-import javax.swing.JOptionPane;
-
 /**
-   This program demonstrates how to use the String.format
+   This program demonstrates how to use the System.out.printf
    method to format a number as currency.
- */
+*/
 
-/**
- * @author jpurba
- *
- */
-public class CurrencyFormat {
-
-	/**
-	 * 
-	 */
-	public CurrencyFormat() {
-		// TODO Auto-generated constructor stub
-	}
-
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class CurrencyFormat
+{
+	public static void main(String[] args)
+	{
 		double monthlyPay = 5000.0;
 		double annualPay = monthlyPay * 12;
-		
-		JOptionPane.showMessageDialog(null,
-			String.format("Your annual pay is $%,.2f", annualPay));
+		System.out.printf("Your annual pay is $%,.2f\n", annualPay);
 	}
-
 }
