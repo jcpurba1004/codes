f06f1db07d5c8c59e1fe7c3e130ebc1897ef45d0
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-01-10 15:11:40 -0800

    Create DieArgument.java example.
    Create DieArgument.java example.

--- java/cisc_190/eclipse/ConstructorDemo/Die/src/Die.java
+++ java/cisc_190/eclipse/ConstructorDemo/Die/src/Die.java
@@ -1,9 +1,57 @@
+import java.util.Random;
 
-public class Die {
+/**
+   The Die class simulates a six-sided die.
+*/
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class Die
+{
+	private int sides; // Number of sides
+	private int value; // The die's value
 
+	/**
+ 	   The constructor performs an initial
+ 	   roll of the die.
+ 	   @param numSides The number of sides for this die.
+	*/
+
+	public Die(int numSides)
+	{
+		sides = numSides;
+		roll();
 	}
 
+	/**
+ 	   The roll method simulates the rolling of
+ 	   the die.
+	*/
+
+	public void roll()
+	{
+		// Create a Random object.
+		Random rand = new Random();
+
+		// Get a random value for the die.
+		value = rand.nextInt(sides) + 1;
+	}
+
+	/**
+ 	   getSides method
+ 	   @return The number of sides for this die.
+	*/
+
+	public int getSides()
+	{
+		return sides;
+	}
+
+	/**
+ 	   getValue method
+ 	   @return The value of the die.
+	*/
+
+	public int getValue()
+	{
+		return value;
+	}
 }
