acd04cef11e8b6a57781aa15181c797342b6d25e
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-01-11 16:43:27 -0800

    Create MetricDemo.java example.
    Create MetricDemo.java example.

--- java/cisc_190/eclipse/Class/Metric/src/Metric.java
+++ java/cisc_190/eclipse/Class/Metric/src/Metric.java
@@ -1,9 +1,30 @@
+/**
+   This class demonstrates static methods.
+*/
 
-public class Metric {
+public class Metric
+{
+	/**
+	   The milesToKilometers method converts a
+	   distance in miles to kilometers.
+	   @param m The distance in miles.
+	   @return The distance in kilometers.
+	*/
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static double milesToKilometers(double m)
+	{
+		return m * 1.609;
 	}
 
+	/**
+ 	   The kilometersToMiles method converts
+ 	   a distance in kilometers to miles.
+ 	   @param k The distance in kilometers.
+ 	   @return The distance in miles.
+	*/
+
+	public static double kilometersToMiles(double k)
+	{
+		return k / 1.609;
+	}
 }
