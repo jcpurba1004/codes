26740dfdd36dd6be3b8d581ac25ef2cecfac8e8b
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-01-13 15:20:19 -0800

    Create PassFailExam.java example.
    Create PassFailExam.java example.

--- java/cisc_190/eclipse/Inheritance/PassFailActivity/src/PassFailActivity.java
+++ java/cisc_190/eclipse/Inheritance/PassFailActivity/src/PassFailActivity.java
@@ -1,9 +1,39 @@
+/**
+   This class holds a numeric score and determines
+   whether the score is passing or failing.
+*/
 
-public class PassFailActivity {
+public class PassFailActivity extends GradedActivity
+{
+	private double minPassingScore; // Minimum passing score
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	/**
+	   The constructor sets the minimum passing score.
+	   @param mps The minimum passing score.
+	*/
 
+	public PassFailActivity(double mps)
+	{
+		minPassingScore = mps;
 	}
 
+	/**
+	   The getGrade method returns a letter grade
+	   determined from the score field. This
+	   method overrides the superclass method.
+	   @return The letter grade.
+	*/
+
+	@Override
+	public char getGrade()
+	{
+		char letterGrade;
+	
+		if (super.getScore() >= minPassingScore)
+		   letterGrade = 'P';
+		else
+		   letterGrade = 'F';
+	
+		return letterGrade;
+	 }
 }
---
+++ java/cisc_190/eclipse/Inheritance/PassFailExam/src/PassFailExam.java
@@ -0,0 +1,9 @@
+
+public class PassFailExam {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
