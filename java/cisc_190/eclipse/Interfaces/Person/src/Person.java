27b825cb61ec9126b745af4c5f9ce8c17513677a
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-01-13 18:52:52 -0800

    Create InterfaceDemo.java example.
    Create InterfaceDemo.java example.

--- java/cisc_190/eclipse/Interfaces/Person/src/Person.java
+++ java/cisc_190/eclipse/Interfaces/Person/src/Person.java
@@ -1,9 +1,16 @@
+public class Person implements Displayable
+{
+	private String name;
 
-public class Person {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// Constructor
+	public Person(String n)
+	{
+		name = n;
 	}
 
+	// display method
+	public void display()
+	{
+		System.out.println("My name is " + name);
+	}
 }
