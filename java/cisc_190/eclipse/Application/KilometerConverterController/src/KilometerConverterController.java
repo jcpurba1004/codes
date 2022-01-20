8c6b41b3adace94d7194aea627d47cb7a570a7f7
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2022-01-20 15:20:13 -0800

    Create RadioButtonDemo.java example.
    Create RadioButtonDemo.java example.

--- java/cisc_190/eclipse/Application/KilometerConverterController/src/KilometerConverterController.java
+++ java/cisc_190/eclipse/Application/KilometerConverterController/src/KilometerConverterController.java
@@ -1,9 +1,40 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
-public class KilometerConverterController {
+public class KilometerConverterController
+{
+	@FXML
+	private Button convertButton;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@FXML
+	private TextField kilometerTextField;
 
+	@FXML
+	private Label outputLabel;
+
+	@FXML
+	private Label promptLabel;
+
+	// This optional method is called when the FXML file is loaded.
+	public void initialize()
+	{
+		// Perform any necessary initialization here.
 	}
+ 
+	// Event listener for the convertButton
+	public void convertButtonListener()
+	{
+		final double CONVERSION_FACTOR = 0.6214;
+ 
+		// Get the kilometers from the TextField.
+		String str = kilometerTextField.getText();
 
+		//Convert kilometers to miles.
+		double miles = Double.parseDouble(str) * CONVERSION_FACTOR;
+ 
+		// Display the converted distance.
+		outputLabel.setText(str + " kilometers is " + miles + " miles.");
+	}
 }
