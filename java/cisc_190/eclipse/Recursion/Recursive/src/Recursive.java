9ff678bb74606b8990afda2fc5129c7d82c5f0d9
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2022-01-21 14:46:15 -0800

    Create RecursionDemo.java example.
    Create RecursionDemo.java example.

--- java/cisc_190/eclipse/Recursion/Recursive/src/Recursive.java
+++ java/cisc_190/eclipse/Recursion/Recursive/src/Recursive.java
@@ -1,9 +1,16 @@
+/**
+   This class has a recursive method, message,
+   which displays a message n times.
+*/
 
-public class Recursive {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Recursive
+{
+	public static void message(int n)
+	{
+		if (n > 0)
+		{
+		   System.out.println("This is a recursive method.");
+		   message(n - 1);
+		}
 	}
-
 }
