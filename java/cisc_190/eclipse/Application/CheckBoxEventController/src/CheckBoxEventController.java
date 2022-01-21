1bb28e68bb68b36320070f57fbcdff4109b1ae44
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2022-01-20 16:01:26 -0800

    Create ImageViewDemo.java example.
    Create ImageViewDemo.java example.

--- java/cisc_190/eclipse/Application/CheckBoxEventController/src/CheckBoxEventController.java
+++ java/cisc_190/eclipse/Application/CheckBoxEventController/src/CheckBoxEventController.java
@@ -1,9 +1,21 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.CheckBox;
 
-public class CheckBoxEventController {
+public class CheckBoxEventController 
+{
+	@FXML
+	private CheckBox myCheckBox;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	@FXML
+	private Label outputLabel;
 
+	// Event listener for myCheckBox
+	public void myCheckBoxListener() 
+	{
+		if (myCheckBox.isSelected())
+		   outputLabel.setText("The CheckBox is selected.");
+		else
+		   outputLabel.setText("The CheckBox is not selected.");
 	}
-
 }
