0a7f6e8652b949c60dd934616f9d1dc3ffafad19
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2022-01-24 14:54:46 -0800

    Create CoffeeDBQuery.java example.
    Create CoffeeDBQuery.java example.

--- java/cisc_190/eclipse/Databases/TableFormatter/src/TableFormatter.java
+++ java/cisc_190/eclipse/Databases/TableFormatter/src/TableFormatter.java
@@ -1,9 +1,36 @@
+import javax.swing.*;
+import java.awt.*;
 
-public class TableFormatter {
+/**
+   The TableFormatter class displays a populated JTable.
+*/
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class TableFormatter extends JFrame
+{
+	// Constants for size.
+	private final int WIDTH = 400;
+	private final int HEIGHT = 200;
 
+	/**
+ 	   Constructor
+	*/
+
+	public TableFormatter(Object[][] data, Object[] colNames)
+	{
+		// Specify an action for the close button.
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+		// Create a JTable with the results.
+		JTable table = new JTable(data, colNames);
+
+		// Put the table in a scroll pane.
+		JScrollPane scrollPane = new JScrollPane(table);
+
+		// Add the table to the content pane.
+		add(scrollPane, BorderLayout.CENTER);
+
+		// Set the size and display.
+		setSize(WIDTH, HEIGHT);
+		setVisible(true);
 	}
-
 }
