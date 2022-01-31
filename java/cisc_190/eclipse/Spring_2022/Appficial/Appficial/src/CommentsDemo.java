d6dda117821fc3c0fe83061b739d6dbff18477a9
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-01-31 15:26:46 -0800

    Create CommentsDemo.java example.
    Create CommentsDemo.java example.

---
+++ java/cisc_190/eclipse/Spring_2022/Appficial/Appficial/src/CommentsDemo.java
@@ -0,0 +1,23 @@
+
+/*
+ * 
+ * this is
+ * a block
+ * style comment
+ */
+public class CommentsDemo { // the class header
+
+	/*
+	 * the main method
+	 */
+	public static void main(String[] args) { // the main method
+		
+		// print out hello world to the screen
+		System.out.println("hello world"); /* java statement */
+		System.out.println("hello world"); // java statement
+		/* comment */ System.out.println("hello world");
+		
+		// end of program
+		
+	} // end of main
+}
