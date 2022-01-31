307a2aff7d88fb6a915b78b66a7eef139c5983b7
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-01-31 15:33:28 -0800

    Create LogicErrorDemo.java example.
    Create LogicErrorDemo.java example.

---
+++ java/cisc_190/eclipse/Spring_2022/Appficial/Appficial/src/LogicErrorDemo.java
@@ -0,0 +1,12 @@
+public class LogicErrorDemo {
+
+	public static void main(String[] args) {
+		
+		int a = 4;
+		int b = 20;
+		
+		int total = a * b;
+		
+		System.out.println("The sum is " + total);
+	}
+}
