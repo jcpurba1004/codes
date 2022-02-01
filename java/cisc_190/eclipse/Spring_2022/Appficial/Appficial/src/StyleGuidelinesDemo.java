daddef93fa9a79d3c4685fad80c36ad530781d39
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-02-01 10:31:51 -0800

    Create StyleGuidelinesDemo.java example.
    Create StyleGuidelinesDemo.java example.

---
+++ java/cisc_190/eclipse/Spring_2022/Appficial/Appficial/src/StyleGuidelinesDemo.java
@@ -0,0 +1,14 @@
+public class StyleGuidelinesDemo {
+
+	public static void main(String[] args) {
+		
+		final double
+		
+		SALES_TAX = 6.0;
+		int age = 5;
+		
+		System.out.println("hello world");
+		System.out.println(age);
+		System.out.println(SALES_TAX);
+	}
+}
