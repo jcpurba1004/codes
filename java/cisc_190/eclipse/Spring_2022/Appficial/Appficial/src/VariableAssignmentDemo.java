54bf940bdd5e252d8ea5f3b13b1e74e0577c740e
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-02-01 09:56:55 -0800

    Create VariableAssignmentDemo.java example.
    Create VariableAssignmentDemo.java example.

---
+++ java/cisc_190/eclipse/Spring_2022/Appficial/Appficial/src/VariableAssignmentDemo.java
@@ -0,0 +1,23 @@
+public class VariableAssignmentDemo {
+
+	public static void main(String[] args) {
+		
+		// dataType variableName = value;
+		
+		int age = 10;
+		
+		
+		int num; // declaring a variable
+		num = 8; // assigning the variable to a value (initializing)
+		num = 9; // not initializing
+		
+		int a, b, c;
+		a = 5;
+		b = 6;
+		c = 7;
+		int x = 1, y = 2, z;
+		
+		System.out.println(age);
+		
+	}
+}
