8108af424b53f1db1e1193ae2ee5229e93f0f851
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-02-01 10:06:44 -0800

    Create IdentifierDemo.java example.
    Create IdentifierDemo.java example.

---
+++ java/cisc_190/eclipse/Spring_2022/Appficial/Appficial/src/IdentifierDemo.java
@@ -0,0 +1,14 @@
+public class IdentifierDemo {
+
+	public static void main(String[] args) {
+		
+		
+		double salesTax = 8.75;
+		
+		int age = 16;
+		
+		int tupac = 2;
+		int ma$e = 12;
+		
+	}
+}
