0bdbcf4c46d0f854b951fd16c6470dbb164e6077
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-02-01 10:17:43 -0800

    Create ConstantDemo.java example.
    Create ConstantDemo.java example.

---
+++ java/cisc_190/eclipse/Spring_2022/Appficial/Appficial/src/ConstantDemo.java
@@ -0,0 +1,11 @@
+public class ConstantDemo {
+
+	public static final double PI = 3.14;
+	public static final double SALES_TAX = 8.75;
+	
+	public static void main(String[] args) {
+		
+		System.out.println(PI);
+		
+	}
+}
