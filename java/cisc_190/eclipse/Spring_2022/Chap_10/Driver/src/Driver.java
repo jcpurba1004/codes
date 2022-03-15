da7996bb5c53ff30c7ecda28cc87bdf74b25dc9c
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-03-15 10:15:27 -0700

    Edit Driver.java and Triangle.java example.
    Edit Driver.java and Triangle.java example.

--- java/cisc_190/eclipse/Spring_2022/Chap_10/Driver/src/Driver.java
+++ java/cisc_190/eclipse/Spring_2022/Chap_10/Driver/src/Driver.java
@@ -1,8 +1,20 @@
+import java.util.ArrayList;
+
 public class Driver {
 
 	public static void main(String[] args) {
 		
+		Triangle t = new Triangle();
 		Square s = new Square();
 		Circle c = new Circle();
+		
+		ArrayList<Shape> shapes = new ArrayList<Shape>();
+		
+		shapes.add(c);
+		shapes.add(t);
+		shapes.add(s);
+		
+		for (int i=0; i<shapes.size(); i++)
+			shapes.get(i).draw();
 	}
 }
