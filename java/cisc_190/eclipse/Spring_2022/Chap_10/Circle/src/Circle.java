0f3936f52f2231b3e66447b1c8675a559e8239fb
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-03-16 10:12:39 -0700

    Create NameAgeChecker.java on Chap_11_6_1 example.
    Create NameAgeChecker.java on Chap_11_6_1 example.

--- java/cisc_190/eclipse/Spring_2022/Chap_10/Circle/src/Circle.java
+++ java/cisc_190/eclipse/Spring_2022/Chap_10/Circle/src/Circle.java
@@ -4,12 +4,4 @@ public class Circle extends Shape {
 	public void draw() {
 		System.out.println("O");
 	}
-	
-	@Override
-	public void calcArea() {
-	}
-	
-	@Override
-	public void calcPerimeter() {
-	}
 }
--- java/cisc_190/eclipse/Spring_2022/Chap_10/Shape/src/Shape.java
+++ java/cisc_190/eclipse/Spring_2022/Chap_10/Shape/src/Shape.java
@@ -1,5 +1,4 @@
 public abstract class Shape {
 
 	public abstract void draw();
-	
 }
