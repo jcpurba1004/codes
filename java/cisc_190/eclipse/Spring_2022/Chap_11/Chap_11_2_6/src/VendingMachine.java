0526d102a46488cdaa7eb65f3b8ed322929ba58c
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-03-18 09:44:10 -0700

    Edit VendingMachine.java on Chap_11_2_6 example.
    Edit VendingMachine.java on Chap_11_2_6 example.

--- java/cisc_190/eclipse/Spring_2022/Chap_11/Chap_11_2_6/src/VendingMachine.java
+++ java/cisc_190/eclipse/Spring_2022/Chap_11/Chap_11_2_6/src/VendingMachine.java
@@ -2,30 +2,30 @@ import java.util.Scanner;
 import java.util.InputMismatchException;
 
 public class VendingMachine {
-        public static void main(String[] args) {
-                Scanner scnr = new Scanner(System.in);
-                int maxNumPeople = 10;
-                int totalNumPeople = 0;
-                // looping as long as totalNumPeople is below maxNumPeople
-                while (totalNumPeople < maxNumPeople) {
-                        // defining a catch block
-                        try {
-                                // reading an integer, adding to totalNumPeople if read is successful
-                                totalNumPeople += scnr.nextInt();
-                                // preventing totalNumPeople going negative. remove this if you don't need.
-                                if (totalNumPeople < 0) {
-                                        totalNumPeople = 0;
-                                }
-                                // displaying occupancy
-                                System.out.println("Occupancy: " + totalNumPeople);
-                        } catch (InputMismatchException e) {
-                                // InputMismatchException is caught. reading off one string
-                                scnr.next();
-                                // printing an error message (remove if not needed)
-                                System.out.println("Error");
-
-                        }
+	
+    public static void main(String[] args) {
+        	
+        Scanner scnr = new Scanner(System.in);
+        int itemNumber = 0;
+        boolean askForInput = true;
+                
+        while(askForInput) {
+            try {
+                itemNumber = scnr.nextInt();
+                if((itemNumber <=0) || (itemNumber > 8)) {
+                	throw new Exception("Try again");
                 }
-                System.out.println("We're full!");
+                System.out.println("Dispensing item " + itemNumber);
+                askForInput = false;
+            }
+            catch (InputMismatchException excpt) {
+                System.out.println("Fatal error");
+                askForInput = false;
+            }
+            catch (Exception excpt) {
+            	System.out.println(excpt.getMessage());
+            	askForInput = true;
+            }
         }
+    }
 }
