efca64fe922d5c5bf331433e2e4905ae6a4c99e4
Jeremiah Purba <54612729+jcpurba1004@users.noreply.github.com>
2022-03-23 17:01:23 -0700

    Create Demo.java on Chap_11 example.
    Create Demo.java on Chap_11 example.

---
+++ java/cisc_190/eclipse/Spring_2022/Chap_11/Demo/src/Demo.java
@@ -0,0 +1,12 @@
+public class Demo {
+
+	public static void main(String[] args) {
+		
+		int[] array = {2, 5, 7, 12, 50, 23};
+		
+		for (int i=0; i<array.length; i++) {
+			System.out.println(array[i]);
+		}
+		System.out.println("the end");
+	}
+}
