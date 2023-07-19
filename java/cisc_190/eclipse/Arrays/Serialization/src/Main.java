36e409331a9dcc032fc1b2ef457d41cbe4f63096
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-07-19 15:36:24 -0700

    Create Main.java on TimerTask example.
    Create Main.java on TimerTask example.

---
+++ java/cisc_190/eclipse/Arrays/Serialization/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Serialization =   The process of converting an object into a byte stream.
+		//                   Persists (saves the state) the object after program exits
+		//                   This byte stream can be saved as a file or sent over a network
+		//                   Can be sent to a different machine
+		//                   Byte stream can be saved as a file (.ser) which is platform independent
+		//                   (Think of this as if you're saving a file with the object's information)
+		
+		// Deserialization = The reverse process of converting a byte stream into an object.
+		//                   (Think of this as if you're loading a saved file)
+	}
+}
