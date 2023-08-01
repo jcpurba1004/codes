e3f11adf07c66b13173f720b77bf48aefb21bce8
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-08-01 12:01:04 -0700

    Create AutoBoxingDemo.java on AutoBoxing example.
    Create AutoBoxingDemo.java on AutoBoxing example.

---
+++ java/cisc_190/eclipse/Autoboxing/Autoboxing/src/AutoBoxingDemo.java
@@ -0,0 +1,23 @@
+public class AutoBoxingDemo {
+	public static void main(String[] args) 
+	{
+		int i = 10;
+		
+		// Autobox
+		Integer iObj = Integer.valueOf(i);
+		System.out.println("Value of Integer obj: " + iObj);
+		
+		// auto-unbox
+		int i1 = iObj;
+		System.out.println("Value of i1: " + i1);
+		
+		char x = 'a';
+		// Autobox
+		Character charObj = Character.valueOf(x);
+		
+		// Auto-unbox
+		char ch = charObj;
+		System.out.println("Value of ch: " + ch);
+		System.out.println("Value of charObj: " + charObj);
+	}
+}
