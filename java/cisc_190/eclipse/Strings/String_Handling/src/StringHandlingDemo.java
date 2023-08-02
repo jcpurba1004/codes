fd55fe4e010563f7e8bda03e59e7ed654b7f542a
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-08-02 14:12:02 -0700

    Create StringHandlingDemo.java on Strings example.
    Create StringHandlingDemo.java on Strings example.

---
+++ java/cisc_190/eclipse/Strings/String_Handling/src/StringHandlingDemo.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class StringHandlingDemo {
+	public static void main(String[] args) {
+		String str1 = "John is studying ";
+		String str2 = "in college";
+		
+//		System.out.println(str1.length());
+		
+//		String result = str1.concat(str2);
+//		System.out.println(result);
+		
+//		String r = String.format("The name of student is " + "%s, and the age is " + "variable is %");
+//		System.out.println(r);
+		
+//		System.out.println(str1.charAt(5));
+		
+//		if(str1.equals(str2)) {
+//			System.out.println("both strings are same");
+//		} else {
+//			System.out.println("both strings are different");
+//		}
+		
+		System.out.println(str1.indexOf('y'));
+		
+//		System.out.println(str1.replace('s', 'r'));
+		
+//		String[] arr = str1.split(" ");
+//		Arrays.asList(arr).forEach(s -> System.out.println(s));
+		
+//		String newStr1 = str1.substring(1, 5);
+//		System.out.println(newStr1);
+	}
+}
