e952d01791a96f78166fbdb930c275bd792e12d0
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-08-02 11:42:12 -0700

    Create SingleGenericHolder.java and DualGenericHolder.java on Generics example.
    Create SingleGenericHolder.java and DualGenericHolder.java on Generics example.

---
+++ java/cisc_190/eclipse/Generics/Generics/src/DualGenericHolder.java
@@ -0,0 +1,20 @@
+public class DualGenericHolder<T, U> {
+	T obj1;
+	U obj2;
+	
+	DualGenericHolder(T obj1, U obj2) {
+		this.obj1 = obj1;
+		this.obj2 = obj2;
+	}
+	
+	public void display() {
+		System.out.println(obj1);
+		System.out.println(obj2);
+	}
+	
+	public static void main(String[] args) {
+		DualGenericHolder<String, Integer> obj = new DualGenericHolder
+				<String, Integer>("SimpliCode", 10);
+		obj.display();
+	}
+}
