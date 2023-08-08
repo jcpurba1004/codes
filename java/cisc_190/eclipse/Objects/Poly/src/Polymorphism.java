d47bcfdfc378ca93661e089470914be52f98f1d0
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-08-08 10:37:53 -0700

    Create Polymorphism.java on Objects example.
    Create Polymorphism.java on Objects example.

---
+++ java/cisc_190/eclipse/Objects/Poly/src/Polymorphism.java
@@ -0,0 +1,19 @@
+class Bird {
+	public void sing() {
+		System.out.println("tweet tweet tweet");
+	}
+}
+
+class Robin extends Bird {
+	public void sing(String song) {
+		System.out.println("twiddledeedee");
+	}
+}
+
+public class Polymorphism {
+
+	public static void main(String[] args) {
+		Robin b = new Robin();
+		b.sing();
+	}
+}
