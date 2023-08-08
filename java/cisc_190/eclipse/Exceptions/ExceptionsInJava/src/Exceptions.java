079ccaf370ab2506ebbd7446c80d9360945fd41e
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-08-08 10:56:44 -0700

    Create Exceptions.java on Exceptions example.
    Create Exceptions.java on Exceptions example.

---
+++ java/cisc_190/eclipse/Exceptions/ExceptionsInJava/src/Exceptions.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Exceptions {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("book");
+		System.out.println(list.get(0));
+	}
+}
