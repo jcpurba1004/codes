898f19caf53e6a9d9c253b3b00d0c94eaf01e115
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-08-30 12:38:37 -0700

    Create ASCIIArt.java on Introduction example.
    Create ASCIIArt.java on Introduction example.

---
+++ java/cisc_190/eclipse/Introduction/ASCII_art/src/ASCIIArt.java
@@ -0,0 +1,11 @@
+public class ASCIIArt {
+   public static void main(String[] args) {
+
+      // Draw tree
+      System.out.println("   *");
+      System.out.println("  ***");
+      System.out.println(" *****");
+      System.out.println("*******");
+      System.out.println("  ***");
+   }
+}
