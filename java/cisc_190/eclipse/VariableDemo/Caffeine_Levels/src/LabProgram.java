56bee0646eb5274cfa02701cfd1ce1fd74746f1c
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-08-30 15:32:29 -0700

    Create Caffeine_Levels on VariableDemo example.
    Create Caffeine_Levels on VariableDemo example.

---
+++ java/cisc_190/eclipse/VariableDemo/Caffeine_Levels/src/LabProgram.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in); 
+      double caffeineMg; // "double" supports floating-point like 75.5, versus int for integers like 75. 
+      
+      caffeineMg = scnr.nextDouble(); 
+     
+     System.out.printf("After 6 hours: %.2f mg\n", caffeineMg / 2);
+     System.out.printf("After 12 hours: %.2f mg\n", caffeineMg / 4);
+     System.out.printf("After 24 hours: %.2f mg\n", caffeineMg / 16);
+     
+     scnr.close();
+   }
+}
