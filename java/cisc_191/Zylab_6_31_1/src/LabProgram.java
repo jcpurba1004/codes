bda304df9c6b04a7e69f2c08be2be60fb15af5f1
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-09-02 13:41:02 -0700

    Create LabProgram.java on Zylab_6_31_1.
    Create LabProgram.java on Zylab_6_31_1.

---
+++ java/cisc_191/Zylab_6_31_1/src/LabProgram.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class LabProgram {
+	public static String coinFlip(Random rand) {
+		int val = rand.nextInt(2);
+		if(val == 0) {
+			return "Tails";
+		}
+		else {
+			return "Heads";
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		Random rand = new Random(2);
+		int n = scnr.nextInt();
+		for(int i = 0; i < n; i++) {
+			System.out.println(coinFlip(rand));
+		}
+	}
+}
