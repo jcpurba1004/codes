0bf87484193e98f8da39df32dedb377c046faad7
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-09-05 11:57:33 -0700

    Create LabProgram.java on Zylab_12_9_1.
    Create LabProgram.java on Zylab_12_9_1.

---
+++ java/cisc_191/Zylab_12_9_1/src/LabProgram.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+
+      try {
+            int userNum = scnr.nextInt();
+            int divNum = scnr.nextInt();
+            
+            int quotient = userNum / divNum;
+            System.out.println(quotient);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception: " + e.getMessage());
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Input Mismatch Exception: " + e.toString());
+        }
+   }
+}
