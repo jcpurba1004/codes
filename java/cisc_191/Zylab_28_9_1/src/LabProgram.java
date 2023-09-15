a41995ef7b4a5dc08d15002dae65ea5303428e6b
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-09-14 23:09:50 -0700

    Create TriangleArea.java and Triangle.java on Zylab_7_23_1.
    Create TriangleArea.java and Triangle.java on Zylab_7_23_1.

---
+++ java/cisc_191/Zylab_28_9_1/src/LabProgram.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class LabProgram {
+	
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		int nums[];
+		int count;
+		int min;
+		int max;
+	}
+}
---
+++ java/cisc_191/Zylab_7_23_1/src/TriangleArea.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+   
+   public static void main(String[] args) {
+       Scanner scnr=new Scanner(System.in);
+      
+       Triangle triangle1=new Triangle();
+       Triangle triangle2=new Triangle();
+      
+       triangle1.setBase(scnr.nextDouble());
+       triangle1.setHeight(scnr.nextDouble());
+       triangle2.setBase(scnr.nextDouble());
+       triangle2.setHeight(scnr.nextDouble());
+       
+       System.out.println("Triangle with larger area");
+       
+       if(triangle1.getArea()>triangle2.getArea())
+           triangle1.printInfo();
+       
+       else
+           triangle2.printInfo();
+       scnr.close();
+   }
+}
