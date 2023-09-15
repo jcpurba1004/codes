a7cad2e2fb008f96598863071e38e51a341b56ba
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-09-15 15:25:05 -0700

    Create NumberPattern.java on Zylab_11_11_1.
    Create NumberPattern.java on Zylab_11_11_1.

--- java/cisc_191/Zylab_11_11_1/src/NumberPattern.java
+++ java/cisc_191/Zylab_11_11_1/src/NumberPattern.java
@@ -1,20 +1,25 @@
 import java.util.Scanner;
 
 public class NumberPattern {
-   System.out.print(num1);
+
+    public static void printNumPattern(int num1, int num2) {
         
-   if (num1 >= 0) {   
+        System.out.print(num1);
+   
+        if (num1 >= 0) {
             System.out.print(" ");
             printNumPattern(num1-num2, num2);
             System.out.print(" "+num1);
-  
-   public static void main(String[] args) { 
-      Scanner scnr = new Scanner(System.in);
-      int num1;
-      int num2;
-      
-      num1 = scnr.nextInt();
-      num2 = scnr.nextInt();
-      printNumPattern(num1, num2); 
-   } 
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int num1;
+        int num2;
+
+        num1 = scnr.nextInt();
+        num2 = scnr.nextInt();
+        printNumPattern(num1, num2);
+    }
 }
