74720fb19e7952d53b1bd6487c5734498537d85c
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-10-02 23:26:16 -0700

    Create LabProgram.java on Zylab_12_8_1.
    Create LabProgram.java on Zylab_12_8_1.

---
+++ java/cisc_191/Zylab_12_7_1/src/LabProgram.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class LabProgram {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      String[] names = { "Ryley", "Edan", "Reagan", "Henry", "Caius", "Jane", "Guto", "Sonya", "Tyrese", "Johnny" };
+      
+      int index;
+		index = scnr.nextInt();
+      
+      try {
+            System.out.println("Name: " + names[index]);
+      }
+      
+     catch (ArrayIndexOutOfBoundsException e) {
+         
+      System.out.println("Exception! "+e.getMessage());
+      if(index<=0){
+          System.out.println("The closest name is: "+names[0]);     
+      } else {
+          System.out.println("The closest name is: "+names[9]);
+      }
+    }
+  }
+}
