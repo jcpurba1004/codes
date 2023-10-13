f3877c5c2c2a8be499cd8423f0af2670f8b0d18e
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-10-12 18:19:21 -0700

    Create LabProgram.java on Zylab_1_15_1.
    Create LabProgram.java on Zylab_1_15_1.

---
+++ java/cisc_191/Zylab_1_15_1/src/LabProgram.java
@@ -0,0 +1,6 @@
+public class LabProgram {
+   public static void main(String[] args) {
+      System.out.println("  NO PARKING");  /*print NO PARKING with two leading spaces*/
+      System.out.println("2:00 - 6:00 a.m.");
+   }
+}
