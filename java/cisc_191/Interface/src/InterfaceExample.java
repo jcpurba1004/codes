5e25139d194a290f021188a774e276a4d48cb2d0
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-11-17 15:32:40 -0800

    Create InterfaceExample.java on Interface example.
    Create InterfaceExample.java on Interface example.

---
+++ java/cisc_191/Interface/src/InterfaceExample.java
@@ -0,0 +1,28 @@
+interface WaterBottleInterface {
+	
+	String color = "Blue";
+	
+	void fillUp();
+	void pourOut();
+}
+
+public class InterfaceExample implements WaterBottleInterface {
+
+	public static void main(String[] args) {
+		System.out.println(color);
+		
+		InterfaceExample ex = new InterfaceExample();
+		ex.fillUp();
+	}
+
+	@Override
+	public void fillUp() {
+		System.out.println("It is filled");
+	}
+
+	@Override
+	public void pourOut() {
+		// TODO Auto-generated method stub
+		
+	}
+}
