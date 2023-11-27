3fd04ee8e48c86d47cf607c4ba73e38897ec0e09
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-11-26 22:45:34 -0800

    Create Main.java, Bicycle.java, Boat.java, Car.java and Vehicle.java on Polymorphism example.
    Create Main.java, Bicycle.java, Boat.java, Car.java and Vehicle.java on Polymorphism example.

---
+++ java/cisc_191/Polymorphism/src/Vehicle.java
@@ -0,0 +1,12 @@
+public class Vehicle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void go() {
+		// TODO Auto-generated method stub
+		
+	}
+}
