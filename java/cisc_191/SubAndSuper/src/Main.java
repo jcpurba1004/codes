f4287c473660906f1f65f9bf5dbc527dcad453ce
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-12-28 23:29:40 -0800

    Create Main.java, Running.java, Shoe.java and Walking.java on Inheritance example.
    Create Main.java, Running.java, Shoe.java and Walking.java on Inheritance example.

---
+++ java/cisc_191/SubAndSuper/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+	public static void main(String[] args) {
+		Running r = new Running(4.5, "Nike", 10);
+		System.out.println(r.weight);
+	}
+}
---
+++ java/cisc_191/SubAndSuper/src/Shoe.java
@@ -0,0 +1,10 @@
+public class Shoe {
+
+	public final String brand;
+	public final double size;
+	
+	Shoe(String brand, double size) {
+		this.brand = brand;
+		this.size = size;
+	}
+}
