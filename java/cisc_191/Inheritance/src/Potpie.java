b6f2d6ab66b5fc99779252bc173ee0743071f652
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2023-12-29 11:22:55 -0800

    Create Apples.java, Food.java, Potpie.java and Tuna.java on Inheritance example.
    Create Apples.java, Food.java, Potpie.java and Tuna.java on Inheritance example.

---
+++ java/cisc_191/Inheritance/src/Potpie.java
@@ -0,0 +1,6 @@
+public class Potpie {
+	
+	public void eat() {
+		System.out.println("I am the eat methods");
+	}
+}
---
+++ java/cisc_191/Inheritance/src/Tuna.java
@@ -0,0 +1,6 @@
+public class Tuna {
+	
+	public void eat() {
+		System.out.println("I am the eat methods");
+	}
+}
