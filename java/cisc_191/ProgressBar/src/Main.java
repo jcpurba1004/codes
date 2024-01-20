cfeabe7c4f6e99918705f6d7a00590b702c43c0e
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2024-01-19 22:44:24 -0800

    Create Main.java and ProgressBarDemo.java on ProgressBar example.
    Create Main.java and ProgressBarDemo.java on ProgressBar example.

---
+++ java/cisc_191/ProgressBar/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// progress bar = Visual aid to let the user know that an operation is processing
+		
+		ProgressBarDemo demo = new ProgressBarDemo();
+	}
+}
