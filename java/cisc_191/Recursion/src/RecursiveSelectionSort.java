0d31a71c2afc8f84f76368596aeaa3fc68163401
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2024-04-15 22:53:45 -0700

    Create DirectorySize.java example.
    Create DirectorySize.java example.

--- java/cisc_191/Recursion/src/RecursiveSelectionSort.java
+++ java/cisc_191/Recursion/src/RecursiveSelectionSort.java
@@ -1,26 +1,26 @@
 public class RecursiveSelectionSort {
- 2    public static void sort(double[] list) {
- 3      sort(list, 0, list.length − 1); // Sort the entire list
- 4    }
- 5
- 6    private static void sort(double[] list, int low, int high) {
- 7      if (low < high) {
- 8        // Find the smallest number and its index in list[low .. high]
- 9        int indexOfMin = low;
-10        double min = list[low];
-11        for (int i = low + 1; i <= high; i++) {
-12          if (list[i] < min) {
-13            min = list[i];
-14            indexOfMin = i;
-15          }
-16        }
-17
-18        // Swap the smallest in list[low .. high] with list[low]
-19        list[indexOfMin] = list[low];
-20        list[low] = min;
-21
-22        // Sort the remaining list[low+1 .. high]
-23        sort(list, low + 1, high);
-24      }
-25    }
-26  }
+	public static void sort(double[] list) {
+		sort(list, 0, list.length − 1); // Sort the entire list
+	}
+
+	private static void sort(double[] list, int low, int high) {
+		if (low < high) {
+			// Find the smallest number and its index in list[low .. high]
+			int indexOfMin = low;
+			double min = list[low];
+			for (int i = low + 1; i <= high; i++) {
+				if (list[i] < min) {
+					min = list[i];
+					indexOfMin = i;
+				}
+			}
+
+			// Swap the smallest in list[low .. high] with list[low]
+			list[indexOfMin] = list[low];
+			list[low] = min;
+
+			// Sort the remaining list[low+1 .. high]
+			sort(list, low + 1, high);
+		}
+	}
+}
