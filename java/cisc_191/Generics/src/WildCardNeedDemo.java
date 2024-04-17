1cd306713375df3ecae665a75e9b14892f14c28c
Jeremiah Purba <Jeremiah.Purba@yahoo.com>
2024-04-16 21:44:21 -0700

    Create WildCardNeedDemo.java example.
    Create WildCardNeedDemo.java example.

---
+++ java/cisc_191/Generics/src/WildCardNeedDemo.java
@@ -0,0 +1,23 @@
+public class WildCardNeedDemo {
+	public static void main(String[] args ) {
+		GenericStack<Integer> intStack = new GenericStack<>();
+		intStack.push(1); // 1 is autoboxed into an Integer object
+		intStack.push(2);
+		intStack.push(−2);
+
+		System.out.print("The max number is " + max(intStack));
+	}
+	
+	/** Find the maximum in a stack of numbers */
+	public static double max(GenericStack<Number> stack) {
+		double max = stack.pop().doubleValue(); // Initialize max
+
+		while (!stack.isEmpty()) {
+			double value = stack.pop().doubleValue();
+			if (value > max)
+				max = value;
+		}
+
+		return max;
+	}
+}
